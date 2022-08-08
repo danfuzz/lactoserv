adf9f34bed6336accc6751ee4c94b51184f4ee98
Dan Bornstein <danfuzz@milk.com>
2022-08-08 14:00:58 -0700

    More testing.

--- src/@milk/just-a-test-2/SomethingElse.js
+++ src/@milk/just-a-test-2/SomethingElse.js
@@ -1,13 +1,11 @@
 // Copyright 2022 Dan Bornstein. All rights reserved.
 // All code and assets are considered proprietary and unlicensed.
 
-import { SomethingElse } from '@milk/just-a-test-2';
 /**
  * Just a test.
  */
 export class Something {
   constructor() {
-    console.log('Yay!');
-    new SomethingElse();
+    console.log('More yay!');
   }
 }
