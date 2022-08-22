d7d80b6971a0eb19b20cd621469ef9ee0c440b34
Dan Bornstein <danfuzz@milk.com>
2022-08-22 15:50:18 -0700

    Validate the config.

--- src/pkg/static-server/private/BaseExportedServer.js
+++ src/pkg/static-server/private/BaseExportedServer.js
@@ -4,6 +4,8 @@
 import { ActualServer } from '#p/ActualServer';
 import { PROTECTED_ACCESS } from '#p/PROTECTED_ACCESS';
 
+import { Validator } from 'jsonschema';
+
 /**
  * Base class for the exported (public) server classes.
  */
@@ -15,11 +17,22 @@ export class BaseExportedServer {
   #actual;
 
   /**
-   * Constructs an instance.
+   * Constructs an instance. Configuration object details:
+   *
+   * * `{string} protocol` -- Name of the protocol to answer to. One of `http`,
+   *   `http2`, or `https`.
+   * * `{string} host` -- Name/address of the interface to listen on. `::` to
+   *   listen on all interfaces.
+   * * `{int} port` -- Port number to listen on.
+   *
+   * Additional configuration for `http2` and `https`:
+   * * `{string} cert` -- Certificate to present.
+   * * `{string} key` -- Private key associated with `cert`.
    *
    * @param {object} config Configuration object.
    */
   constructor(config) {
+    BaseExportedServer.#validateConfig(config);
     this.#actual = new ActualServer(config);
   }
 
@@ -59,5 +72,79 @@ export class BaseExportedServer {
    */
   async whenStopped() {
     return this.#actual.whenStopped();
+  }
+
+  /**
+   * Validates the given configuration object.
+   *
+   * @param {object} config Configuration object.
+   */
+  static #validateConfig(config) {
+    const v = new Validator();
+
+    const options = {
+      throwAll: true
+    };
+
+    // See <https://json-schema.org/>.
+    const schema = {
+      allOf: [
+        {
+          title: 'server-core',
+          type: 'object',
+          required: ['protocol', 'host', 'port'],
+          properties: {
+            protocol: {
+              type: 'string',
+              enum: ['http', 'http2', 'https']
+            },
+            host: {
+              type: 'string'
+            },
+            port: {
+              type: 'integer',
+              minimum: 1,
+              maximum: 65535
+            }
+          },
+        },
+        {
+          title: 'secure-server',
+          if: {
+            type: 'object',
+            properties: {
+              protocol: {
+                type: 'string',
+                enum: ['http2', 'https']
+              }
+            }
+          },
+          then: {
+            type: 'object',
+            required: ['cert', 'key'],
+            properties: {
+              cert: {
+                type: 'string'
+              },
+              key: {
+                type: 'string'
+              }
+            }
+          }
+        }
+      ]
+    };
+
+    const result = v.validate(config, schema);
+    const errors = result.errors;
+
+    if (errors.length != 0) {
+      console.log('Configuration error%s:', (errors.length == 1) ? '' : 's');
+      for (const e of errors) {
+        console.log('  %s', e.stack);
+      }
+
+      throw new Error('Invalid configuration.');
+    }
   }
 }
