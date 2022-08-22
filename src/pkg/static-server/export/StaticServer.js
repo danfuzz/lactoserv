3123dad996fd8fca2c9aef293fe4aba27051954d
Dan Bornstein <danfuzz@milk.com>
2022-08-22 16:06:00 -0700

    Pull temporary assets out of `static-server`.
    ...and pass in the path in the config, and validate that config.

--- src/pkg/static-server/export/StaticServer.js
+++ src/pkg/static-server/export/StaticServer.js
@@ -6,32 +6,70 @@ import { BaseExportedServer } from '#p/BaseExportedServer';
 import { PROTECTED_ACCESS } from '#p/PROTECTED_ACCESS';
 
 import express from 'express';
-
-import * as url from 'url';
+import { Validator } from 'jsonschema';
 
 /**
  * Static content server.
  */
 export class StaticServer extends BaseExportedServer {
   /**
-   * Constructs an instance.
+   * Constructs an instance. Configuration object details:
+   *
+   * * `{string} assetsPath` -- Base directory for the static assets.
    *
    * @param {object} config Configuration object.
    */
   constructor(config) {
     super(config);
-    this.#addRoutes();
+    StaticServer.#validateConfig(config);
+
+    this.#addRoutes(config.assetsPath);
   }
 
   /**
    * Adds routes to the application instance.
+   *
+   * @param {string} assetsPath Base directory for the static assets.
    */
-  #addRoutes() {
+  #addRoutes(assetsPath) {
     const actual = this.getActual(PROTECTED_ACCESS)
     const app = actual.app;
 
-    // TODO: Way more stuff. For now, just serve some static files.
-    const assetsDir = url.fileURLToPath(new URL('../assets', import.meta.url));
-    app.use('/', express.static(assetsDir))
+    app.use('/', express.static(assetsPath))
+  }
+
+  /**
+   * Validates the given configuration object.
+   *
+   * @param {object} config Configuration object.
+   */
+  static #validateConfig(config) {
+    const v = new Validator();
+
+    const schema = {
+      title: 'static-server',
+      type: 'object',
+      required: ['what', 'assetsPath'],
+      properties: {
+        what: {
+          const: 'static-server'
+        },
+        assetsPath: {
+          type: 'string'
+        }
+      }
+    };
+
+    const result = v.validate(config, schema);
+    const errors = result.errors;
+
+    if (errors.length != 0) {
+      console.log('Configuration error%s:', (errors.length == 1) ? '' : 's');
+      for (const e of errors) {
+        console.log('  %s', e.stack);
+      }
+
+      throw new Error('Invalid configuration.');
+    }
   }
 }
