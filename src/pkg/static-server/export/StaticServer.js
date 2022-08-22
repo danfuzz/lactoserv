4e93ef200af57e8aa196bee29f6de399ec23707e
Dan Bornstein <danfuzz@milk.com>
2022-08-22 13:24:54 -0700

    Pull out will-be-common code, and do some access control.

--- src/pkg/static-server/export/StaticServer.js
+++ src/pkg/static-server/export/StaticServer.js
@@ -2,6 +2,7 @@
 // All code and assets are considered proprietary and unlicensed.
 
 import { ActualServer } from '#p/ActualServer';
+import { BaseExportedServer } from '#p/BaseExportedServer';
 
 import express from 'express';
 
@@ -10,48 +11,23 @@ import * as url from 'url';
 /**
  * Static content server.
  */
-export class StaticServer {
-  /** {ActualServer} Underlying server instance. */
-  #actual;
-
+export class StaticServer extends BaseExportedServer {
   /**
    * Constructs an instance.
    *
    * @param {object} config Configuration object.
    */
   constructor(config) {
-    this.#actual = new ActualServer(config);
+    super(config);
     this.#addRoutes();
-  }
-
-  /**
-   * Starts the server.
-   */
-  async start() {
-    await this.#actual.start();
-  }
-
-  /**
-   * Stops the server.
-   */
-  async stop() {
-    return this.#actual.stop();
-  }
-
-  /**
-   * Returns when the server becomes stopped (stops listening / closes its
-   * server socket). In the case of closing due to an error, this throws the
-   * error.
-   */
-  async whenStopped() {
-    return this.#actual.whenStopped();
   }
 
   /**
    * Adds routes to the application instance.
    */
   #addRoutes() {
-    const app = this.#actual.app;
+    const actual = this.getActual(BaseExportedServer.ACCESS_TOKEN)
+    const app = actual.app;
 
     // TODO: Way more stuff. For now, just serve some static files.
     const assetsDir = url.fileURLToPath(new URL('../assets', import.meta.url));
---
+++ src/pkg/static-server/private/BaseExportedServer.js
@@ -0,0 +1,78 @@
+// Copyright 2022 Dan Bornstein. All rights reserved.
+// All code and assets are considered proprietary and unlicensed.
+
+import { ActualServer } from '#p/ActualServer';
+
+import express from 'express';
+
+import * as url from 'url';
+
+/**
+ * Base class for the exported (public) server classes.
+ */
+export class BaseExportedServer {
+  /** {object} Unique object used to grant access to innards. */
+  static #ACCESS_TOKEN = Symbol(this.name);
+
+  /** {object} Access token for innards. */
+  #accessToken;
+
+  /** {ActualServer} Underlying server instance. */
+  #actual;
+
+  /** {object} Unique object used to grant access to innards. */
+  static get ACCESS_TOKEN() {
+    return this.#ACCESS_TOKEN;
+  }
+
+  /**
+   * Constructs an instance.
+   *
+   * @param {object} config Configuration object.
+   */
+  constructor(config) {
+    this.#actual = new ActualServer(config);
+  }
+
+  /**
+   * Gets the internal `ActualServer` instance, but only if this method is
+   * presented with the designated access token.
+   *
+   * This arrangement in effect makes this a "protected method" in the usual
+   * sense. (More specifically, it's usable by subclasses in this module but not
+   * by clients of this module.)
+   *
+   * @param {object} acccessToken Access token.
+   * @returns {ActualServer} Underlying server instance.
+   */
+  getActual(accessToken) {
+    if (accessToken !== BaseExportedServer.#ACCESS_TOKEN) {
+      throw new Error('Access token mismatch.');
+    }
+
+    return this.#actual;
+  }
+
+  /**
+   * Starts the server.
+   */
+  async start() {
+    await this.#actual.start();
+  }
+
+  /**
+   * Stops the server.
+   */
+  async stop() {
+    return this.#actual.stop();
+  }
+
+  /**
+   * Returns when the server becomes stopped (stops listening / closes its
+   * server socket). In the case of closing due to an error, this throws the
+   * error.
+   */
+  async whenStopped() {
+    return this.#actual.whenStopped();
+  }
+}
