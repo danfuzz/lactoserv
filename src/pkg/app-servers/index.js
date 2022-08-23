4efcf3d97dd587d29fd91507e742902def773b06
Dan Bornstein <danfuzz@milk.com>
2022-08-23 12:45:29 -0700

    Much less boilerplatey!

--- src/pkg/app-servers/index.js
+++ src/pkg/app-servers/index.js
@@ -1,7 +1,5 @@
 // Copyright 2022 Dan Bornstein. All rights reserved.
 // All code and assets are considered proprietary and unlicensed.
 
-import { RedirectServer } from '#x/RedirectServer';
-import { StaticServer } from '#x/StaticServer';
-
-export { RedirectServer, StaticServer };
+export * from '#x/RedirectServer';
+export * from '#x/StaticServer';
