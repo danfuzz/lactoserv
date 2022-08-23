fba56dac95cb84f04062a1a5fad4bd73c5a84d4a
Dan Bornstein <danfuzz@milk.com>
2022-08-23 12:37:12 -0700

    New class `RedirectServer`.

--- src/pkg/app-servers/index.js
+++ src/pkg/app-servers/index.js
@@ -1,6 +1,7 @@
 // Copyright 2022 Dan Bornstein. All rights reserved.
 // All code and assets are considered proprietary and unlicensed.
 
+import { RedirectServer } from '#x/RedirectServer';
 import { StaticServer } from '#x/StaticServer';
 
-export { StaticServer };
+export { RedirectServer, StaticServer };
