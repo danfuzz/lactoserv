fd97eb8beb6a792abe40830226542f75dc0de165
Dan Bornstein <danfuzz@milk.com>
2022-08-23 12:45:42 -0700

    Much less boilerplatey!

--- src/pkg/main/index.js
+++ src/pkg/main/index.js
@@ -1,6 +1,4 @@
 // Copyright 2022 Dan Bornstein. All rights reserved.
 // All code and assets are considered proprietary and unlicensed.
 
-import { Main } from '#x/Main';
-
-export { Main };
+export * from '#x/Main';
