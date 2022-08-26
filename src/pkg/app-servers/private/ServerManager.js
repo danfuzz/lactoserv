5072936f3f64b5a8cd0b7d1ab911e8f1a825a15e
Dan Bornstein <danfuzz@milk.com>
2022-08-26 11:15:23 -0700

    Split out `ServerController` from `ServerManager`.

--- src/pkg/app-servers/private/ServerManager.js
+++ src/pkg/app-servers/private/ServerManager.js
@@ -1,6 +1,8 @@
 // Copyright 2022 Dan Bornstein. All rights reserved.
 // All code and assets are considered proprietary and unlicensed.
 
+import { ServerController } from '#p/ServerController';
+
 import { Validator } from 'jsonschema';
 
 /**
@@ -181,64 +183,5 @@ export class ServerManager {
 
       throw new Error('Invalid configuration.');
     }
-  }
-}
-
-/**
- * "Controller" for a single server.
- */
-class ServerController {
-  /** {string} Server name. */
-  #name;
-
-  /** {string} Interface address. */
-  #interface;
-
-  /** {int} Port number. */
-  #port;
-
-  /** {string} Protocol. */
-  #protocol;
-
-  /**
-   * Constructs an insance.
-   *
-   * @param {object} serverConfig Server information configuration item.
-   */
-  constructor(serverConfig) {
-    this.#name      = serverConfig.name;
-    this.#interface = serverConfig.interface;
-    this.#port      = serverConfig.port;
-    this.#protocol  = serverConfig.protocol;
-  }
-
-  /** {object} Plain object which recapitulates the original configuration. */
-  get configObject() {
-    return {
-      name:      this.#name,
-      interface: this.#interface,
-      port:      this.#port,
-      protocol:  this.#protocol
-    }
-  }
-
-  /** {string} Server name. */
-  get name() {
-    return this.#name;
-  }
-
-  /** {string} Interface address. */
-  get interface() {
-    return this.#interface;
-  }
-
-  /** {int} Port number. */
-  get port() {
-    return this.#port;
-  }
-
-  /** {string} Protocol. */
-  get protocol() {
-    return this.#protocol;
   }
 }
