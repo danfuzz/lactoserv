fcf41f93a976c201b955ee5b5223e8351e3e5b62
Dan Bornstein <danfuzz@milk.com>
2022-08-26 11:18:10 -0700

    Split out `HostController` from `HostManager`.

---
+++ src/pkg/app-servers/private/HostController.js
@@ -0,0 +1,61 @@
+// Copyright 2022 Dan Bornstein. All rights reserved.
+// All code and assets are considered proprietary and unlicensed.
+
+import * as tls from 'node:tls';
+
+/**
+ * "Controller" for a single host entry, which can notably offer services for
+ * multiple different hosts.
+ */
+export class HostController {
+  /** {string[]} List of hostnames, including partial or full wildcards. */
+  #names;
+
+  /** {string} Certificate, in PEM form. */
+  #cert;
+
+  /** {string} Key, in PEM form. */
+  #key;
+
+  /** {SecureContext} TLS context representing this instance's info. */
+  #secureContext;
+
+  /**
+   * Constructs an insance.
+   *
+   * @param {object} hostConfig Host configuration item.
+   */
+  constructor(hostConfig) {
+    const nameArray = hostConfig.name ? [hostConfig.name] : [];
+    const namesArray = hostConfig.names ?? [];
+    this.#names = [...nameArray, ...namesArray];
+
+    this.#cert = hostConfig.cert;
+    this.#key = hostConfig.key;
+
+    this.#secureContext = tls.createSecureContext({
+      cert: this.#cert,
+      key:  this.#key
+    });
+  }
+
+  /** {string[]} List of hostnames, including partial or full wildcards. */
+  get names() {
+    return this.#names;
+  }
+
+  /** {string} Certificate, in PEM form. */
+  get cert() {
+    return this.#cert;
+  }
+
+  /** {string} Key, in PEM form. */
+  get key() {
+    return this.#key;
+  }
+
+  /** {SecureContext} TLS context representing this instance's info. */
+  get secureContext() {
+      return this.#secureContext;
+  }
+}
--- src/pkg/app-servers/private/HostManager.js
+++ src/pkg/app-servers/private/HostManager.js
@@ -1,7 +1,7 @@
 // Copyright 2022 Dan Bornstein. All rights reserved.
 // All code and assets are considered proprietary and unlicensed.
 
-import * as tls from 'node:tls';
+import { HostController } from '#p/HostController';
 
 import { Validator } from 'jsonschema';
 
@@ -313,62 +313,5 @@ export class HostManager {
 
       throw new Error('Invalid configuration.');
     }
-  }
-}
-
-/**
- * "Controller" for a single host entry, which can notably offer services for
- * multiple different hosts.
- */
-class HostController {
-  /** {string[]} List of hostnames, including partial or full wildcards. */
-  #names;
-
-  /** {string} Certificate, in PEM form. */
-  #cert;
-
-  /** {string} Key, in PEM form. */
-  #key;
-
-  /** {SecureContext} TLS context representing this instance's info. */
-  #secureContext;
-
-  /**
-   * Constructs an insance.
-   *
-   * @param {object} hostConfig Host configuration item.
-   */
-  constructor(hostConfig) {
-    const nameArray = hostConfig.name ? [hostConfig.name] : [];
-    const namesArray = hostConfig.names ?? [];
-    this.#names = [...nameArray, ...namesArray];
-
-    this.#cert = hostConfig.cert;
-    this.#key = hostConfig.key;
-
-    this.#secureContext = tls.createSecureContext({
-      cert: this.#cert,
-      key:  this.#key
-    });
-  }
-
-  /** {string[]} List of hostnames, including partial or full wildcards. */
-  get names() {
-    return this.#names;
-  }
-
-  /** {string} Certificate, in PEM form. */
-  get cert() {
-    return this.#cert;
-  }
-
-  /** {string} Key, in PEM form. */
-  get key() {
-    return this.#key;
-  }
-
-  /** {SecureContext} TLS context representing this instance's info. */
-  get secureContext() {
-      return this.#secureContext;
   }
 }
