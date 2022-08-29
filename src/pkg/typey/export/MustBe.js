a519fb9c5ce0b72b6ce2c0522fa1a5fa5281c449
Dan Bornstein <danfuzz@milk.com>
2022-08-29 14:02:50 -0700

    New class `MustBe`.

---
+++ src/pkg/typey/export/MustBe.js
@@ -0,0 +1,22 @@
+// Copyright 2022 Dan Bornstein. All rights reserved.
+// All code and assets are considered proprietary and unlicensed.
+
+/**
+ * Simple type assertions that can be placed at the starts of methods.
+ */
+export class MustBe {
+  /**
+   * Checks for type `boolean`.
+   *
+   * @param {*} value Arbitrary value.
+   * @returns {boolean} `value` if it is of type `boolean`.
+   * @throws {Error} Thrown if `value` is of any other type.
+   */
+  boolean(value) {
+    if (typeof value === 'boolean') {
+      return value;
+    }
+
+    throw new Error('Must be of type `boolean`.');
+  }
+}
--- src/pkg/typey/index.js
+++ src/pkg/typey/index.js
@@ -2,3 +2,4 @@
 // All code and assets are considered proprietary and unlicensed.
 
 export * from '#x/Methods';
+export * from '#x/MustBe';
