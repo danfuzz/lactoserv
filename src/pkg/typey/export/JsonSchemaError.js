f99c4e6d48971ad398a7cdecb1dc5daf88b9f490
Dan Bornstein <danfuzz@milk.com>
2022-08-30 14:39:49 -0700

    Better error reporting (just a little).

--- src/pkg/typey/export/JsonSchemaError.js
+++ src/pkg/typey/export/JsonSchemaError.js
@@ -1,6 +1,8 @@
 // Copyright 2022 Dan Bornstein. All rights reserved.
 // All code and assets are considered proprietary and unlicensed.
 
+import * as util from 'node:util';
+
 /**
  * Error result from use of {@link JsonSchema}.
  */
@@ -40,18 +42,19 @@ export class JsonSchemaError {
     for (const e of this.#errors) {
       const origPath = e.instancePath.replace(/^[/]/, '').split('/');
       const path = [];
+
       for (const component of origPath) {
         if (component.match(/^[0-9]+$/)) {
           path.push(`[${component}]`);
         } else if (path.length === 0) {
-          path.push(component);
+          path.push(component === '' ? '<top>' : component);
         } else {
           path.push(`.${component}`);
         }
       }
 
-      console.log('  %s:\n    %s\n    got: %o',
-        path.join(''), e.message, e.data);
+      const got = JsonSchemaError.#errorStringFor(e.data, 9);
+      console.log('  %s:\n    %s\n    got: %s', path.join(''), e.message, got);
     }
 
     logger();
@@ -74,5 +77,38 @@ export class JsonSchemaError {
   #getMessage() {
     const count = this.#errors.length;
     return `${count} error${count === 1 ? '' : 's'} in ${this.#title}`;
+  }
+
+
+  //
+  // Static members
+  //
+
+  /**
+   * Gets a possibly-trimmed string for the given data, for use in producing
+   * error messages.
+   *
+   * @param {*} data Data to stringify.
+   * @param {number} indent How much to indent all but the first line
+   * @returns {string} Stringified version.
+   */
+  static #errorStringFor(data, indent) {
+    const fullString = util.format('%o', data);
+    const lines = fullString.split('\n');
+
+    if (lines.length === 1) {
+      return fullString;
+    }
+
+    if (lines.length > 3) {
+      lines.splice(3, lines.length - 3, '...');
+    }
+
+    const spaces = ' '.repeat(indent);
+    for (let i = 1; i < lines.length; i++) {
+      lines[i] = `\n${spaces}${lines[i]}`;
+    }
+
+    return lines.join('');
   }
 }
