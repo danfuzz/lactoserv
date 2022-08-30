2610bf8822911a1e103154142aa9e994b898f68e
Dan Bornstein <danfuzz@milk.com>
2022-08-30 12:08:21 -0700

    Apparently ESLint doesn't use `@link`s to determine usage.

--- src/pkg/typey/export/JsonSchemaError.js
+++ src/pkg/typey/export/JsonSchemaError.js
@@ -1,8 +1,6 @@
 // Copyright 2022 Dan Bornstein. All rights reserved.
 // All code and assets are considered proprietary and unlicensed.
 
-import { JsonSchema } from '#x/JsonSchema';
-
 /**
  * Error result from use of {@link JsonSchema}.
  */
