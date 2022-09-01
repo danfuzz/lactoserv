968c6111c55b45bca826991a2f7b60373ca7e349
Dan Bornstein <danfuzz@milk.com>
2022-09-01 10:31:10 -0700

    New version of `find()`.

--- src/pkg/app-servers/private/TreePathMap.js
+++ src/pkg/app-servers/private/TreePathMap.js
@@ -80,6 +80,32 @@ export class TreePathMap {
   /**
    * Finds the most-specific binding for the given path.
    *
+   * @param {TreePathKey|{path: string[], wildcard: boolean}} key Key to look
+   *   up. If `.wildcard` is `true`, then this method will only find bindings
+   *   which are wildcards, though they might be more general than the `.path`
+   *   being looked for.
+   * @returns {?{key: TreePathKey, pathRemainder: string[], value: *}}
+   *   Information about the found result, or `null` if there was no match at
+   *   all.
+   *   * `key` -- The key that was matched.
+   *   * `pathSuffix` -- The portion of `path` that was matched by a wildcard,
+   *     if this was in fact a wildcard match.
+   *   * `value` -- The bound value that was found.
+   */
+  find(key) {
+    const { path, wildcard } = key;
+
+    if (! (key instanceof TreePathKey)) {
+      MustBe.arrayOfString(path);
+      MustBe.boolean(wildcard);
+    }
+
+    return this.#find0(path, wildcard);
+  }
+
+  /**
+   * Finds the most-specific binding for the given path.
+   *
    * @param {string[]} path Path to look up.
    * @param {boolean} [wildcard = false] Must the result be a wildcard binding?
    * @returns {?{path: string[], pathRemainder: string[], value: *,
@@ -113,26 +139,23 @@ export class TreePathMap {
       if (subtree.#hasEmpty && !wildcard) {
         // There's an exact match for the path.
         return {
-          path:          [... path],
-          pathRemainder: [],
-          value:         subtree.#emptyValue,
-          wildcard:      false
+          key:        new TreePathKey(path, false),
+          pathSuffix: [],
+          value:      subtree.#emptyValue
         };
       } else if (subtree.#hasWildcard) {
         // There's a matching wildcard at the end of the path.
         return {
-          path:          [... path],
-          pathRemainder: [],
-          value:         subtree.#wildcardValue,
-          wildcard:      true
+          key:        new TreePathKey(path, true),
+          pathSuffix: [],
+          value:      subtree.#wildcardValue,
         };
       }
     } else if (foundIndex >= 0) {
       return {
-        path:          path.slice(0, foundIndex),
-        pathRemainder: path.slice(foundIndex),
-        value:         foundValue,
-        wildcard:      true
+        key:        new TreePathKey(path.slice(0, foundIndex), true),
+        pathSuffix: path.slice(foundIndex),
+        value:      foundValue
       };
     }
 
@@ -204,6 +227,60 @@ export class TreePathMap {
       }
       subtree.#add0(path, wildcard, value, pathIndex + 1);
     }
+  }
+
+  /**
+   * Helper for {@link #find}, which does most of the work.
+   *
+   * @param {string[]} path Path to look up.
+   * @param {boolean} wildcard Must the result be a wildcard binding?
+   * @returns {?object} Result as described by {@link #find}.
+   */
+  #find0(path, wildcard) {
+    let subtree = this;
+    let foundIndex = -1;
+    let foundValue = null;
+
+    let at;
+    for (at = 0; at < path.length; at++) {
+      if (subtree.#hasWildcard) {
+        foundValue = subtree.#wildcardValue;
+        foundIndex = at;
+      }
+      subtree = subtree.#subtrees.get(path[at]);
+      if (!subtree) {
+        break;
+      }
+    }
+
+    if (at === path.length) {
+      if (subtree.#hasEmpty && !wildcard) {
+        // There's an exact match for the path.
+        return {
+          path:          [... path],
+          pathRemainder: [],
+          value:         subtree.#emptyValue,
+          wildcard:      false
+        };
+      } else if (subtree.#hasWildcard) {
+        // There's a matching wildcard at the end of the path.
+        return {
+          path:          [... path],
+          pathRemainder: [],
+          value:         subtree.#wildcardValue,
+          wildcard:      true
+        };
+      }
+    } else if (foundIndex >= 0) {
+      return {
+        path:          path.slice(0, foundIndex),
+        pathRemainder: path.slice(foundIndex),
+        value:         foundValue,
+        wildcard:      true
+      };
+    }
+
+    return null;
   }
 
   /**
