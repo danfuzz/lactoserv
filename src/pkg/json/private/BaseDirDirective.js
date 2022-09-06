d0fa30d54568ce72a4ef9601a9d31aaaf49e923e
Dan Bornstein <danfuzz@milk.com>
2022-09-06 11:55:33 -0700

    New directive `$baseDir`, ready for action!

---
+++ src/pkg/json/private/BaseDirDirective.js
@@ -0,0 +1,58 @@
+// Copyright 2022 Dan Bornstein. All rights reserved.
+// All code and assets are considered proprietary and unlicensed.
+
+import { JsonDirective } from '#x/JsonDirective';
+
+import { MustBe } from '@this/typey';
+
+/**
+ * Directive `$baseDir`, for defining a base directory in the filesystem for the
+ * resolution of relative filesystem paths in other directives.
+ */
+export class BaseDirDirective extends JsonDirective {
+  /** {?string} The base directory, if known. */
+  #baseDir = null;
+
+  // Note: The default constructor is fine here.
+
+  /**
+   * @returns {string} The base directory.
+   */
+  get value() {
+    if (this.#baseDir === null) {
+      throw new Error('Base directory not (yet) known.');
+    }
+
+    return this.#baseDir;
+  }
+
+  /** @override */
+  process(pass, path, value) {
+    if (pass !== 1) {
+      return { same: true };
+    }
+
+    if (path.length === 1) {
+      MustBe.string(value, /^[/].*[^/]/);
+      this.#baseDir = value;
+      return { delete: true };
+    } else {
+      throw new Error(`\`${BaseDirDirective.NAME}\` only allowed at top level.`);
+    }
+  }
+
+
+  //
+  // Static members
+  //
+
+  /** @override */
+  static get NAME() {
+    return '$baseDir';
+  }
+
+  /** @override */
+  static get REQUIRES() {
+    return Object.freeze([]);
+  }
+}
