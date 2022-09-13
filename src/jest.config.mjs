a1bc0de21eb53c846e238c7239bb217ed53031d2
Dan Bornstein <danfuzz@milk.com>
2022-09-13 10:05:01 -0700

    Bit of work on making coverage work.
    It doesn't work yet.

--- src/jest.config.mjs
+++ src/jest.config.mjs
@@ -17,20 +17,24 @@ export default {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
+  //collectCoverage: true,
+  collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: [
+    "!**/tests/**",
+    "!**/node_modules/**",
+    "**/@this/**",
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "../../out/coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: [],
 
   // Indicates which provider should be used to instrument code for coverage
+  //coverageProvider: "babel",
   coverageProvider: "v8",
 
   // A list of reporter names that Jest uses when writing coverage reports
