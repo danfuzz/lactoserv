9d3d8ef52c1bb4cc2d6a02f13c8c698fd3d8030b
Dan Bornstein <danfuzz@milk.com>
2022-09-15 17:23:18 -0700

    New class `ChainedEvent`.

--- src/pkg/async/index.js
+++ src/pkg/async/index.js
@@ -1,6 +1,7 @@
 // Copyright 2022 Dan Bornstein. All rights reserved.
 // All code and assets are considered proprietary and unlicensed.
 
+export * from '#x/ChainedEvent';
 export * from '#x/Condition';
 export * from '#x/ManualPromise';
 export * from '#x/Mutex';
