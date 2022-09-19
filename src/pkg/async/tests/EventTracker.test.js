caaeb572fb5ff4f3cfd46c4a33c1637288d99bbc
Dan Bornstein <danfuzz@milk.com>
2022-09-19 12:03:49 -0700

    Add tests.

--- src/pkg/async/tests/EventTracker.test.js
+++ src/pkg/async/tests/EventTracker.test.js
@@ -15,7 +15,18 @@ describe('constructor(ChainedEvent)', () => {
     expect(() => new EventTracker(new ChainedEvent('woop'))).not.toThrow();
   });
 
-  // TODO: something about .headPromise and .headNow.
+  test('makes an instance with the given first event as the `headNow`', () => {
+    const event   = new ChainedEvent('boop');
+    const tracker = new EventTracker(event);
+    expect(tracker.headNow).toBe(event);
+  });
+
+  test('makes an instance with a `headPromise` that promptly resolves to the given first event', async () => {
+    const event   = new ChainedEvent('boop');
+    const tracker = new EventTracker(event);
+
+    await expect(tracker.headPromise).resolves.toBe(event);
+  });
 });
 
 describe('constructor(Promise)', () => {
@@ -24,7 +35,17 @@ describe('constructor(Promise)', () => {
     expect(() => new EventTracker(mp.promise)).not.toThrow();
   });
 
-  // TODO: something about .headPromise and .headNow.
+  test('makes an instance with `headNow === null` (at first)', () => {
+    const eventProm = Promise.resolve(new ChainedEvent('boop'));
+    const tracker   = new EventTracker(eventProm);
+    expect(tracker.headNow).toBeNull();
+  });
+
+  test('makes an instance with a non-null `headPromise`', () => {
+    const eventProm = Promise.resolve(new ChainedEvent('boop'));
+    const tracker   = new EventTracker(eventProm);
+    expect(tracker.headPromise).not.toBeNull();
+  });
 
   test('succeeds given a promise that resolves to a valid value', async () => {
     const mp      = new ManualPromise();
@@ -72,7 +93,39 @@ describe('.headPromise', () => {
 });
 
 describe('.headNow', () => {
-  // TODO
+  test('becomes non-`null` promptly after `headPromise` resolves', async () => {
+    const mp      = new ManualPromise();
+    const tracker = new EventTracker(mp.promise);
+    const event   = new ChainedEvent(payload1);
+
+    expect(tracker.headNow).toBeNull();
+    mp.resolve(event);
+
+    const race = await Promise.race([tracker.headPromise, timers.setTimeout(100)]);
+    expect(race).toBe(event);
+  });
+
+  test('synchronously follows along an already-resolved chain as it is `advance()`d.', () => {
+    const event3  = new ChainedEvent(payload3);
+    const event2  = new ChainedEvent(payload2, event3);
+    const event1  = new ChainedEvent(payload1, event2);
+    const tracker = new EventTracker(event1);
+
+    expect(tracker.headNow).toBe(event1);
+    tracker.advance();
+    expect(tracker.headNow).toBe(event2);
+    tracker.advance();
+    expect(tracker.headNow).toBe(event3);
+  });
+
+  test('becomes `null` after `advance()`ing past the end of the chain.', () => {
+    const event   = new ChainedEvent(payload1);
+    const tracker = new EventTracker(event);
+
+    expect(tracker.headNow).toBe(event);
+    tracker.advance();
+    expect(tracker.headNow).toBeNull();
+  })
 });
 
 describe('advance()', () => {
