27dfc2d157f91fdabe861c7d0948a81a45dbb473
Dan Bornstein <danfuzz@milk.com>
2022-09-26 10:34:24 -0700

    Add `earliestEvent`.

--- src/pkg/see-all/export/Logger.js
+++ src/pkg/see-all/export/Logger.js
@@ -27,6 +27,9 @@ export class Logger {
   /** @type {EventSource} Manager of all the events being emitted. */
   #eventSource;
 
+  /** @type {number} Number of events logged by this instance. */
+  #count = 0;
+
   /**
    * Constructs an instance.
    *
@@ -45,6 +48,19 @@ export class Logger {
   }
 
   /**
+   * @returns {LogEvent} The earliest available event of this logger. When an
+   * instance is first constructed, this is its "kickoff" event; but after the
+   * 100th event is logged, this starts tracking the latest event logged by this
+   * instance. The idea here is that it should take no longer than the time to
+   * log that many events for something to get itself hooked up to this instance
+   * to start processing events, and we don't want to miss out on flushing out
+   * the initial events.
+   */
+  get earliestEvent() {
+    return this.#kickoffEvent ?? this.#eventSource.currentEvent;
+  }
+
+  /**
    * Emits an event from this instance's source.
    *
    * @param {string} type The type of event which is being logged.
@@ -57,6 +73,11 @@ export class Logger {
     const event = new LogEvent(stack, nowSec, this.#tag, type, Object.freeze(args));
 
     this.#eventSource.emit(event);
+    this.#count++;
+
+    if (this.#count >= Logger.COUNT_WHEN_KICKOFF_DROPPED) {
+      this.#kickoffEvent = null;
+    }
   }
 
 
@@ -66,4 +87,10 @@ export class Logger {
 
   /** @type {StdLoggingEnvironment} The default logging environment. */
   static #DEFAULT_ENVIRONMENT = new StdLoggingEnvironment();
+
+  /**
+   * @type {number} Number of events after which {@link #kickoffEvent} is no
+   * longer available.
+   */
+  static #COUNT_WHEN_KICKOFF_DROPPED = 100;
 }
