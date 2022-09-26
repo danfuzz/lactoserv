2a7714cf97b3a0df0a18313d1f9e677c74a979b3
Dan Bornstein <danfuzz@milk.com>
2022-09-26 12:33:56 -0700

    Add tests.

--- src/pkg/async/tests/EventSink.test.js
+++ src/pkg/async/tests/EventSink.test.js
@@ -1,10 +1,11 @@
 // Copyright 2022 Dan Bornstein. All rights reserved.
 // All code and assets are considered proprietary and unlicensed.
 
-import { ChainedEvent, EventSink, PromiseState } from '@this/async';
+import { ChainedEvent, EventSink, PromiseState, PromiseUtil } from '@this/async';
 
 import * as timers from 'node:timers/promises';
-
+import { writeFileSync } from 'node:fs';
+const stderr = s => writeFileSync('/dev/stderr', `${s}\r\n`);
 
 const payload1 = { type: 'wacky' };
 const payload2 = { type: 'zany' };
@@ -56,5 +57,92 @@ describe('constructor(function, promise)', () => {
   test('trivially succeeds, given a promise that is rejected', () => {
     const promise = Promise.reject(new Error('oy!'));
     expect(() => new EventSink(() => true, promise)).not.toThrow();
+  });
+});
+
+describe('run()', () => {
+  test('processes the first event when it is synchronously known', async () => {
+    let callCount   = 0;
+    let callGot     = null;
+    const processor = (event) => {
+      callGot = event;
+      callCount++;
+    };
+
+    const event = new ChainedEvent(payload1);
+    const sink  = new EventSink(processor, event);
+
+    const runResult = sink.run();
+
+    await timers.setImmediate();
+    expect(callCount).toBe(1);
+    expect(callGot).toBe(event);
+
+    sink.stop();
+    expect(await runResult).toBeNull();
+  });
+
+  test('processes the first event when it is promise that resolves to an event', async () => {
+    let callCount   = 0;
+    let callGot     = null;
+    const processor = (event) => {
+      callGot = event;
+      callCount++;
+    };
+
+    const event = new ChainedEvent(payload1);
+    const sink = new EventSink(processor, Promise.resolve(event));
+
+    const runResult = sink.run();
+
+    await timers.setImmediate();
+    expect(callCount).toBe(1);
+    expect(callGot).toBe(event);
+
+    sink.stop();
+    expect(await runResult).toBeNull();
+  });
+
+  test('processes the first "event" when it is promise that is rejected', async () => {
+    let callCount   = 0;
+    const processor = (event_unused) => {
+      callCount++;
+    };
+
+    const reason = new Error('Oh forsooth!');
+    const sink   = new EventSink(processor, Promise.reject(reason));
+
+    const runResult = sink.run();
+    PromiseUtil.handleRejection(runResult);
+
+    await timers.setImmediate();
+    expect(callCount).toBe(0);
+
+    sink.stop();
+    await expect(runResult).rejects.toBe(reason);
+  });
+
+  test('stops in reaction to the processor throwing', async () => {
+    let callCount   = 0;
+    let callGot     = null;
+    const reason    = new Error('Oh for the love of muffins!');
+    const processor = (event) => {
+      callGot = event;
+      callCount++;
+      throw reason;
+    };
+
+    const event = new ChainedEvent(payload1);
+    const sink  = new EventSink(processor, event);
+
+    const runResult = sink.run();
+    PromiseUtil.handleRejection(runResult);
+
+    await timers.setImmediate();
+    expect(callCount).toBe(1);
+    expect(callGot).toBe(event);
+
+    sink.stop();
+    await expect(runResult).rejects.toBe(reason);
   });
 });
