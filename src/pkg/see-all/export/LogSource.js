3a373b9ca3da3747842ca1459d036643b4685826
Dan Bornstein <danfuzz@milk.com>
2022-09-27 09:55:15 -0700

    Last licks (I think) for `LogEvent` -> `LogRecord` etc.

--- src/pkg/see-all/export/LogSource.js
+++ src/pkg/see-all/export/LogSource.js
@@ -3,7 +3,7 @@
 
 import { LogRecord } from '#x/LogRecord';
 
-import { EventSource } from '@this/async';
+import { ChainedEvent, EventSource } from '@this/async';
 import { MustBe } from '@this/typey';
 
 /**
@@ -11,51 +11,45 @@ import { MustBe } from '@this/typey';
  */
 export class LogSource extends EventSource {
   /** @type {?ChainedEvent} The "kickoff" event, if still available. */
-  #kickoffEvent = LogRecord.makeKickoffInstance();
+  #kickoffEvent = new ChainedEvent(LogRecord.makeKickoffInstance());
 
   /** @type {number} Number of events logged by this instance. */
   #count = 0;
 
   /**
    * Constructs an instance.
-   *
-   * @param {LogTag} tag Tag to use on all logged events.
-   * @param {BaseLoggingEnvironment} [environment = null] Logging environment to
-   *   use (it's the source for timestamps and stack traces), or `null` to use
-   *   the default one which is hooked up to the "real world."
    */
-  constructor(tag, environment = null) {
-    super(this.#kickoffEvent);
+  constructor() {
+    const kickoffEvent = new ChainedEvent(LogRecord.makeKickoffInstance());
+    super(kickoffEvent);
+
+    this.#kickoffEvent = kickoffEvent;
   }
 
   /**
-   * @returns {} The earliest available event from this instance. When
-   * an instance is first constructed, this is its "kickoff" event; but after
-   * the 100th event is logged, this starts tracking the latest logged event.
-   * The idea here is that it should take no longer than the time to log that
-   * many events for something to get itself hooked up to this instance to start
-   * processing events, and we don't want to miss out on flushing out the
+   * @returns {ChainedEvent} The earliest available event from this instance.
+   * When an instance is first constructed, this is its "kickoff" event; but
+   * after the 100th event is logged, this starts tracking the latest logged
+   * event. The idea here is that it should take no longer than the time to log
+   * that many events for something to get itself hooked up to this instance to
+   * start processing events, and we don't want to miss out on flushing out the
    * initial events.
    */
   get earliestEvent() {
-    return this.#kickoffEvent ?? this.#eventSource.currentEvent;
+    return this.#kickoffEvent ?? this.currentEvent;
   }
 
   /**
    * Emits an event from this instance's source.
    *
-   * @param {string} type The type of event which is being logged.
-   * @param {...*} args Arbitrary -- generally speaking, defined per-type --
-   *   arguments associated with the event.
+   * @param {LogRecord} record The payload of the event to emit.
    */
-  log(type, ...args) {
-    const stack  = this.#environment.stackTrace();
-    const nowSec = this.#environment.nowSec();
-    const event = new (stack, nowSec, this.#tag, type, Object.freeze(args));
+  emit(record) {
+    MustBe.object(record, LogRecord);
 
-    this.#eventSource.emit(event);
+    super.emit(record);
+
     this.#count++;
-
     if (this.#count >= LogSource.COUNT_WHEN_KICKOFF_DROPPED) {
       this.#kickoffEvent = null;
     }
