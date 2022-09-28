b224d74da27784410dc5520bcbb3855de3bb2a6c
Dan Bornstein <danfuzz@milk.com>
2022-09-27 17:00:04 -0700

    Add tests.

--- src/pkg/async/tests/Threadoid.test.js
+++ src/pkg/async/tests/Threadoid.test.js
@@ -1,11 +1,30 @@
 // Copyright 2022 Dan Bornstein. All rights reserved.
 // All code and assets are considered proprietary and unlicensed.
 
-import { Threadoid } from '@this/async';
+import { PromiseState, Threadoid } from '@this/async';
 
 describe('constructor(function)', () => {
   test('trivially succeeds', () => {
     expect(() => new Threadoid(() => null)).not.toThrow();
+  });
+
+  test('produces an instance for which `isRunning() === false`', () => {
+    const thread = new Threadoid(() => null);
+
+    expect(thread.isRunning()).toBeFalse();
+  });
+
+  test('produces an instance for which `shouldStop() === true`', () => {
+    const thread = new Threadoid(() => null);
+
+    expect(thread.shouldStop()).toBeTrue();
+  });
+
+  test('produces an instance for which `whenStopRequested()` is pre-fulfilled', async () => {
+    const thread = new Threadoid(() => null);
+
+    const result = thread.whenStopRequested();
+    expect(PromiseState.isFulfilled(result)).toBeTrue();
   });
 });
 
