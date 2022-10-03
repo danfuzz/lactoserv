91c1a360a00995b9ceb218ad1f81927c877b6b7c
Dan Bornstein <danfuzz@milk.com>
2022-10-03 09:09:28 -0700

    Add tests.

--- src/pkg/async/tests/Threadoid.test.js
+++ src/pkg/async/tests/Threadoid.test.js
@@ -257,7 +257,7 @@ describe('isStarted()', () => {
   `('$label', ({ useStartFunc }) => {
     const startArg = useStartFunc
       ? [() => null]
-      : []
+      : [];
 
     test('returns `false` before being started', async () => {
       const thread = new Threadoid(...startArg, () => null);
@@ -392,142 +392,224 @@ describe('isStarted()', () => {
 });
 
 describe('run()', () => {
-  test('causes the main function to be called', async () => {
-    let called = false;
-    const thread = new Threadoid(() => {
-      called = true;
-    });
+  describe.each`
+    useStartFunc | label
+    ${false}     | ${'without a start function'}
+    ${true}      | ${'with a start function'}
+  `('$label', ({ useStartFunc }) => {
+    const startArg = useStartFunc
+      ? [() => null]
+      : [];
 
-    const runResult = thread.run();
-    await timers.setImmediate();
-    expect(called).toBeTrue();
-
-    await expect(runResult).toResolve();
-  });
-
-  test('causes the main function to be called, fully asynchronously', async () => {
-    let called = false;
-    const thread = new Threadoid(() => {
-      called = true;
-    });
-
-    const runResult = thread.run();
-    expect(called).toBeFalse();
-    await timers.setImmediate();
-    expect(called).toBeTrue();
-
-    await expect(runResult).toResolve();
-  });
-
-  test('causes the main function to be called, with the thread as its argument', async () => {
-    let gotArgs = null;
-    const thread = new Threadoid((...args) => {
-      gotArgs = args;
-    });
-
-    const runResult = thread.run();
-    await timers.setImmediate();
-    expect(gotArgs).toStrictEqual([thread]);
-
-    await expect(runResult).toResolve();
-  });
-
-  test('causes the main function to be called, with `this` unbound', async () => {
-    let gotThis = null;
-    const thread = new Threadoid(function () {
-      gotThis = this;
-    });
-
-    const runResult = thread.run();
-    await timers.setImmediate();
-    expect(gotThis).toBeUndefined();
-
-    await expect(runResult).toResolve();
-  });
-
-  describe('when called while already running', () => {
-    test('does not call the main function more than once', async () => {
-      let shouldRun = true;
-      let count     = 0;
-      const thread = new Threadoid(async () => {
-        count++;
-        while (shouldRun) {
-          await timers.setImmediate();
-        }
+    test('causes the main function to be called', async () => {
+      let called = false;
+      const thread = new Threadoid(...startArg, () => {
+        called = true;
       });
 
-      const runResult1 = thread.run();
+      const runResult = thread.run();
       await timers.setImmediate();
-      expect(count).toBe(1);
+      expect(called).toBeTrue();
 
-      const runResult2 = thread.run();
-      await timers.setImmediate();
-      expect(count).toBe(1);
-
-      const runResult3 = thread.run();
-      await timers.setImmediate();
-      expect(count).toBe(1);
-
-      shouldRun = false;
-      await expect(runResult1).toResolve();
-      await expect(runResult2).toResolve();
-      await expect(runResult3).toResolve();
-      expect(count).toBe(1);
+      await expect(runResult).toResolve();
     });
 
-    test('returns the same value from all calls during the same run', async () => {
-      let shouldRun = true;
-      let count     = 0;
-      const thread = new Threadoid(async () => {
-        count++;
-        const result = `count-was-${count}`;
-        while (shouldRun) {
-          await timers.setImmediate();
-        }
-
-        return result;
+    test('causes the main function to be called, fully asynchronously', async () => {
+      let called = false;
+      const thread = new Threadoid(...startArg, () => {
+        called = true;
       });
 
-      const runResult1 = thread.run();
-      const runResult2 = thread.run();
+      const runResult = thread.run();
+      expect(called).toBeFalse();
       await timers.setImmediate();
-      const runResult3 = thread.run();
+      expect(called).toBeTrue();
 
-      shouldRun = false;
-      expect(await runResult1).toBe('count-was-1');
-      expect(await runResult2).toBe('count-was-1');
-      expect(await runResult3).toBe('count-was-1');
+      await expect(runResult).toResolve();
     });
 
-    test('causes a second run after a first run completes', async () => {
-      let shouldRun = true;
-      let count     = 0;
-      const thread = new Threadoid(async () => {
-        count++;
-        const result = `count-was-${count}`;
-        while (shouldRun) {
-          await timers.setImmediate();
-        }
-
-        return result;
+    test('causes the main function to be called, with the thread as its argument', async () => {
+      let gotArgs = null;
+      const thread = new Threadoid(...startArg, (...args) => {
+        gotArgs = args;
       });
 
-      const runResult1 = thread.run();
+      const runResult = thread.run();
       await timers.setImmediate();
-      shouldRun = false;
-      await timers.setImmediate();
-      expect(thread.isRunning()).toBeFalse(); // Baseline expectation.
+      expect(gotArgs).toStrictEqual([thread]);
 
-      shouldRun = true;
-      const runResult2 = thread.run();
-      await timers.setImmediate();
-      expect(thread.isRunning()).toBeTrue();
-      shouldRun = false;
-      await timers.setImmediate();
-      expect(thread.isRunning()).toBeFalse();
+      await expect(runResult).toResolve();
+    });
 
-      expect(await runResult1).toBe('count-was-1');
-      expect(await runResult2).toBe('count-was-2');
+    test('causes the main function to be called, with `this` unbound', async () => {
+      let gotThis = null;
+      const thread = new Threadoid(...startArg, function () {
+        gotThis = this;
+      });
+
+      const runResult = thread.run();
+      await timers.setImmediate();
+      expect(gotThis).toBeUndefined();
+
+      await expect(runResult).toResolve();
+    });
+
+    describe('when called while already running', () => {
+      test('does not call the main function more than once', async () => {
+        let shouldRun = true;
+        let count     = 0;
+        const thread = new Threadoid(...startArg, async () => {
+          count++;
+          while (shouldRun) {
+            await timers.setImmediate();
+          }
+        });
+
+        const runResult1 = thread.run();
+        await timers.setImmediate();
+        expect(count).toBe(1);
+
+        const runResult2 = thread.run();
+        await timers.setImmediate();
+        expect(count).toBe(1);
+
+        const runResult3 = thread.run();
+        await timers.setImmediate();
+        expect(count).toBe(1);
+
+        shouldRun = false;
+        await expect(runResult1).toResolve();
+        await expect(runResult2).toResolve();
+        await expect(runResult3).toResolve();
+        expect(count).toBe(1);
+      });
+
+      test('returns the same value from all calls during the same run', async () => {
+        let shouldRun = true;
+        let count     = 0;
+        const thread = new Threadoid(...startArg, async () => {
+          count++;
+          const result = `count-was-${count}`;
+          while (shouldRun) {
+            await timers.setImmediate();
+          }
+
+          return result;
+        });
+
+        const runResult1 = thread.run();
+        const runResult2 = thread.run();
+        await timers.setImmediate();
+        const runResult3 = thread.run();
+
+        shouldRun = false;
+        expect(await runResult1).toBe('count-was-1');
+        expect(await runResult2).toBe('count-was-1');
+        expect(await runResult3).toBe('count-was-1');
+      });
+
+      test('causes a second run after a first run completes', async () => {
+        let shouldRun = true;
+        let count     = 0;
+        const thread = new Threadoid(...startArg, async () => {
+          count++;
+          const result = `count-was-${count}`;
+          while (shouldRun) {
+            await timers.setImmediate();
+          }
+
+          return result;
+        });
+
+        const runResult1 = thread.run();
+        await timers.setImmediate();
+        shouldRun = false;
+        await timers.setImmediate();
+        expect(thread.isRunning()).toBeFalse(); // Baseline expectation.
+
+        shouldRun = true;
+        const runResult2 = thread.run();
+        await timers.setImmediate();
+        expect(thread.isRunning()).toBeTrue();
+        shouldRun = false;
+        await timers.setImmediate();
+        expect(thread.isRunning()).toBeFalse();
+
+        expect(await runResult1).toBe('count-was-1');
+        expect(await runResult2).toBe('count-was-2');
+      });
+    });
+  });
+});
+
+// These are extra tests for `run()` with a start function. The `describe()`s
+// here are set up so the tests get reported sensibly with the ones above.
+describe('run()', () => {
+  describe('with a start function', () => {
+    test('causes the start function to be called, fully asynchronously', async () => {
+      let called = false;
+      const thread = new Threadoid(() => { called = true; }, () => null);
+
+      const runResult = thread.run();
+      expect(called).toBeFalse();
+      await timers.setImmediate();
+      expect(called).toBeTrue();
+
+      await expect(runResult).toResolve();
+    });
+
+    test('causes the start function to be called, with the thread as its argument', async () => {
+      let gotArgs = null;
+      const thread = new Threadoid((...args) => { gotArgs = args; }, () => null);
+
+      const runResult = thread.run();
+      await timers.setImmediate();
+      expect(gotArgs).toStrictEqual([thread]);
+
+      await expect(runResult).toResolve();
+    });
+
+    test('causes the start function to be called, with `this` unbound', async () => {
+      let gotThis = null;
+      const thread = new Threadoid(function () { gotThis = this; }, () => null);
+
+      const runResult = thread.run();
+      await timers.setImmediate();
+      expect(gotThis).toBeUndefined();
+
+      await expect(runResult).toResolve();
+    });
+
+    describe('when called while already running', () => {
+      test('does not call the start function more than once', async () => {
+        let shouldRun = true;
+        let count     = 0;
+        const startFn = async () => {
+          count++;
+          while (shouldRun) {
+            await timers.setImmediate();
+          }
+        };
+        const thread = new Threadoid(startFn, () => null);
+
+        const runResult1 = thread.run();
+        await timers.setImmediate();
+        expect(count).toBe(1);
+
+        const runResult2 = thread.run();
+        await timers.setImmediate();
+        expect(count).toBe(1);
+
+        const runResult3 = thread.run();
+        await timers.setImmediate();
+        expect(count).toBe(1);
+
+        shouldRun = false;
+        await expect(runResult1).toResolve();
+        await expect(runResult2).toResolve();
+        await expect(runResult3).toResolve();
+        expect(count).toBe(1);
+      });
     });
   });
 });
