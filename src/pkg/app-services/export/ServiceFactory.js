be02b7088033127be0aee8e411920105abb7c2ca
Dan Bornstein <danfuzz@milk.com>
2022-10-07 11:19:34 -0700

    First cut of `app-services` module.
    More to come!

---
+++ src/pkg/app-services/export/ServiceFactory.js
@@ -0,0 +1,48 @@
+// Copyright 2022 Dan Bornstein. All rights reserved.
+// All code and assets are considered proprietary and unlicensed.
+
+import { BaseApplication } from '#p/BaseApplication';
+
+
+/**
+ * Utility class which constructs of concrete {@link BaseApplication} instances.
+ */
+export class ApplicationFactory {
+  /**
+   * @type {Map<string, function(new:BaseService, ...*)>} Map from each service
+   * type to the service subclass that implements it.
+   */
+  static #SERVICE_CLASSES = new Map();
+
+  /**
+   * Registers a type/application binding.
+   *
+   * @param {function(new:BaseService, ...*)} serviceClass Application
+   *   class.
+   */
+  static register(serviceClass) {
+    const type = serviceClass.TYPE;
+
+    if (this.#SERVICE_CLASSES.has(type)) {
+      throw new Error(`Already registered: ${type}`);
+    }
+
+    this.#SERVICE_CLASSES.set(type, serviceClass);
+  }
+
+  /**
+   * Constructs an instance of the given service type.
+   *
+   * @param {string} type Type name of the service.
+   * @param {...*} rest Construction arguments.
+   * @returns {BaseService} Constructed service instance.
+   */
+  static forType(type, ...rest) {
+    const cls = this.#SERVICE_CLASSES.get(type);
+    if (!cls) {
+      throw new Error(`Unknown service type: ${type}`);
+    }
+
+    return new cls(...rest);
+  }
+}
