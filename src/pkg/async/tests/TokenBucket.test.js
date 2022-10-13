75fa6d9a3ba7cb0d72bbd1ab1cd921eee0facc04
Dan Bornstein <danfuzz@milk.com>
2022-10-12 18:15:57 -0700

    Add a couple more cases.

--- src/pkg/async/tests/TokenBucket.test.js
+++ src/pkg/async/tests/TokenBucket.test.js
@@ -26,6 +26,16 @@ describe('constructor()', () => {
   test.each`
     opts
     ${{ capacity: 1, fillRate: 1 }}
+    ${{ capacity: 1, fillRate: 1, initialVolume: 0 }}
+    ${{ capacity: 1, fillRate: 1, initialVolume: 1 }}
+    ${{ capacity: 10, fillRate: 1, initialVolume: 10 }}
+    ${{ capacity: 10, fillRate: 1, initialVolume: 9 }}
+    ${{ capacity: 1, fillRate: 1, partialTokens: false }}
+    ${{ capacity: 1, fillRate: 1, partialTokens: true }}
+    ${{ capacity: 1, fillRate: 1, timeSource: new TokenBucket.StdTimeSource() }}
+    ${{ capacity: 1, fillRate: 1, timeSource: new MockTimeSource() }}
+    ${{ capacity: 1, fillRate: 1, initialVolume: 0.5, partialTokens: true,
+        timeSource: new MockTimeSource() }}
     TODO: more
   `('trivially accepts valid options: $opts', ({ opts }) => {
     expect(() => new TokenBucket(opts)).not.toThrow();
