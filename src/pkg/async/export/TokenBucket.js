59f6c37cd38d355a7741d21a6fadceda7d009a67
Dan Bornstein <danfuzz@milk.com>
2022-10-13 09:01:48 -0700

    Fix doc.

--- src/pkg/async/export/TokenBucket.js
+++ src/pkg/async/export/TokenBucket.js
@@ -105,7 +105,7 @@ export class TokenBucket {
    * * `{number} volume` -- The volume, that is, the quantity of tokens, in the
    *   bucket.
    *
-   * @return {object} Snapshot, as described above.
+   * @returns {object} Snapshot, as described above.
    */
   snapshotNow() {
     this.#topUpBucket();
