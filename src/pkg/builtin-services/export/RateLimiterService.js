b30a96c2270cba6b59e0fb1b32d88e7b3c6471de
Dan Bornstein <danfuzz@milk.com>
2022-10-13 11:07:02 -0700

    Do some actual rate limiting.

--- src/pkg/builtin-services/export/RateLimiterService.js
+++ src/pkg/builtin-services/export/RateLimiterService.js
@@ -2,6 +2,7 @@
 // All code and assets are considered proprietary and unlicensed.
 
 import { BaseService, ServiceController } from '@this/app-services';
+import { TokenBucket } from '@this/async';
 import { JsonSchema } from '@this/json';
 
 
@@ -26,14 +27,14 @@ import { JsonSchema } from '@this/json';
  *  `second`, or `msec` (millisecond).
  */
 export class RateLimiterService extends BaseService {
-  /** @type {?*} Connection rate limiter, if any. */
+  /** @type {?TokenBucket} Connection rate limiter, if any. */
   #connections = null;
 
-  /** @type {?*} Request rate limiter, if any. */
-  #requests = null;
-
-  /** @type {?*} Outgoing data rate limiter, if any. */
+  /** @type {?TokenBucket} Outgoing data rate limiter, if any. */
   #data = null;
+
+  /** @type {?TokenBucket} Request rate limiter, if any. */
+  #requests = null;
 
   /**
    * Constructs an instance.
@@ -46,19 +47,60 @@ export class RateLimiterService extends BaseService {
     const config = controller.config;
     RateLimiterService.#validateConfig(config);
 
-    // TODO
+    const { connections, data, requests } = config;
+
+    this.#connections = RateLimiterService.#makeBucket(connections);
+    this.#data        = RateLimiterService.#makeBucket(data);
+    this.#requests    = RateLimiterService.#makeBucket(requests);
   }
 
-  // TODO
+  /**
+   * Waits if necessary, and async-returns when either the caller has been
+   * granted a new connection or there is too much load to grant a connection.
+   *
+   * @param {?function(*)} logger Logger to use for this action.
+   * @returns {boolean} Was a connection actually granted?
+   */
+  async newConnection(logger) {
+    if (!this.#connections) {
+      return true;
+    }
+
+    // TODO: Move the wait/retry logic into `TokenBucket` itself, and make it
+    // less clunky.
+    let totalWaitTime = 0;
+    let waitTime      = 0;
+    for (let i = 0; i < 10; i++) {
+      if (waitTime) {
+        logger?.rateLimited({ attempt: i + 1, waitTime });
+        await this.#connections.wait(waitTime);
+        totalWaitTime += waitTime;
+      }
+
+      const got = this.#connections.takeNow(1);
+      if (got.grant === 1) {
+        if (i !== 0) {
+          logger?.rateLimited({ totalAttempts: i + 1, totalWaitTime })
+        }
+        return true;
+      }
+
+      waitTime = got.waitTime;
+    }
+
+    return false;
+  }
+
+  // TODO: More!
 
   /** @override */
   async start() {
-    // TODO
+    // Nothing to do here.
   }
 
   /** @override */
   async stop() {
-    // TODO
+    // Nothing to do here.
   }
 
 
@@ -69,6 +111,46 @@ export class RateLimiterService extends BaseService {
   /** @returns {string} Service type as used in configuration objects. */
   static get TYPE() {
     return 'rate-limiter';
+  }
+
+  /**
+   * Converts a specified-unit flow rate to one that is per-second.
+   *
+   * @param {number} flowRate The flow rate.
+   * @param {string} timeUnit The time unit for the given `fillRate`.
+   * @return {number} `flowRate` converted to tokens per second.
+   */
+  static #fillRatePerSecFrom(flowRate, timeUnit) {
+    switch (timeUnit) {
+      case 'day':    return flowRate * (1 / (60 * 60 * 24));
+      case 'hour':   return flowRate * (1 / (60 * 60));
+      case 'minute': return flowRate * (1 / 60);
+      case 'second': return flowRate;               // No conversion needed.
+      case 'msec':   return flowRate * 1000;
+      default: {
+        throw new Error(`Unknown time unit: ${timeUnit}`);
+      }
+    }
+  }
+
+  /**
+   * Makes a bucket instance based on the given configuration, or returns `null`
+   * if given `null`.
+   *
+   * @param {?object} config Bucket config, or `null` for "no bucket."
+   * @returns {?TokenBucket} Appropriately-constructed bucket, or `null` if
+   *   `config === null`.
+   */
+  static #makeBucket(config) {
+    if (!config) {
+      return null;
+    }
+
+    const { burstSize, flowRate: origFlowRate, timeUnit } = config;
+    const capacity = burstSize;
+    const fillRate = this.#fillRatePerSecFrom(origFlowRate, timeUnit);
+
+    return new TokenBucket({ capacity, fillRate });
   }
 
   /**
