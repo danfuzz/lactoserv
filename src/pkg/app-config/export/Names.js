01c6def1663286137e73917eef39ab491c23bdb6
Dan Bornstein <danfuzz@milk.com>
2022-10-24 09:36:20 -0700

    Stub out a new module, `app-config`.

---
+++ src/pkg/app-config/export/Names.js
@@ -0,0 +1,12 @@
+// Copyright 2022 Dan Bornstein. All rights reserved.
+// All code and assets are considered proprietary and unlicensed.
+
+import { MustBe } from '@this/typey';
+
+
+/**
+ * Utilities for parsing various sorts of names.
+ */
+export class Names {
+
+}
---
+++ src/pkg/app-config/index.js
@@ -0,0 +1,5 @@
+// Copyright 2022 Dan Bornstein. All rights reserved.
+// All code and assets are considered proprietary and unlicensed.
+
+export * from '#x/Names';
+export * from '#x/Paths';
