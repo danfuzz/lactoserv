d7cd6f0549ca9a313232135e5a69dd85bf5bd83f
Dan Bornstein <danfuzz@milk.com>
2022-10-25 15:10:57 -0700

    Use a config class for the redirector.

--- src/pkg/builtin-apps/export/RedirectApplication.js
+++ src/pkg/builtin-apps/export/RedirectApplication.js
@@ -1,11 +1,7 @@
 // Copyright 2022 Dan Bornstein. All rights reserved.
 // All code and assets are considered proprietary and unlicensed.
 
-import { URL } from 'node:url';
-
-import express from 'express';
-
-import { ApplicationItem } from '@this/app-config';
+import { ApplicationItem, Uris } from '@this/app-config';
 import { ApplicationController, BaseApplication } from '@this/app-servers';
 import { JsonSchema } from '@this/json';
 
@@ -15,20 +11,13 @@ import { JsonSchema } from '@this/json';
  *
  * Configuration object details:
  *
- * * `{array} redirects` -- List of redirections to perform. Each element is
- *   an object with mappings for:
- *   * `{string} fromPath` -- Absolute URI path to trigger off of (no protocol,
- *     host, or port; just path components). Must end with a slash. Can be just
- *     a single slash to redirect all paths.
- *   * `{string} toUri` -- Absolute URI (including protocol, host, and port[*])
- *     of the prefix to redirect to. Must end with a slash. Protocol must be
- *     either `http` or `https`.
- *
- * [*] unless implied by the protocol.
+ * * `{string} target` -- Absolute base URI to redirect to. The relative path of
+ *   the incoming request (based on the mount point of the application) is
+ *   appended to this value to become the full redirected URI.
  */
 export class RedirectApplication extends BaseApplication {
-  /** @type {express.Router} Router with all the redirects. */
-  #router;
+  /** @type {string} The target base URI. */
+  #target;
 
   /**
    * Constructs an instance.
@@ -39,14 +28,14 @@ export class RedirectApplication extends BaseApplication {
   constructor(config, controller) {
     super(config, controller);
 
-    const extraConfig = controller.config.extraConfig;
-    RedirectApplication.#validateConfig(extraConfig);
-    this.#router = RedirectApplication.#makeRouter(extraConfig);
+    // Drop the final slash from `target`, because we'll always be appending a
+    // path that _starts_ with a slash.
+    this.#target = config.target.match(/^(?<target>.*)[/]$/).groups.target;
   }
 
   /** @override */
   handleRequest(req, res, next) {
-    this.#router(req, res, next);
+    res.redirect(`${this.#target}${req.path}`);
   }
 
 
@@ -54,8 +43,9 @@ export class RedirectApplication extends BaseApplication {
   // Static members
   //
 
+  /** @override */
   static get CONFIG_CLASS() {
-    return ApplicationItem;
+    return this.#Config;
   }
 
   /** @override */
@@ -64,128 +54,26 @@ export class RedirectApplication extends BaseApplication {
   }
 
   /**
-   * Makes a request router for an instance of this class.
-   *
-   * @param {object} config Configuration object.
-   * @returns {function(...*)} The middleware function.
+   * Configuration item subclass for this (outer) class.
    */
-  static #makeRouter(config) {
-    const router = express.Router();
-    const redirects = config.redirects;
+  static #Config = class Config extends ApplicationItem {
+    /** @type {string} The target base URI. */
+    #target;
 
-    // Returns a literal-string matching regex suitable for use with an Express
-    // router. We do this because the user-supplied `fromPath`s might
-    // inadvertently use characters from Express's "path pattern" syntax, and we
-    // aren't trying to expose this Express facility. Rather than _just_ detect
-    // if Express might be confused, we simply _always_ convert the path to
-    // a regex. This doesn't cost anything at runtime, because Express itself
-    // always converts paths to regexes anyway. That said and FWIW, see
-    // <https://github.com/pillarjs/path-to-regexp> for details on the path
-    // syntax used by Express.
-    function literalPath(p) {
-      p = p.replaceAll(/[.?*+^$(){}\\|]|\[|\]/g, '\\$&');
-      return new RegExp(p);
+    /**
+     * Constructs an instance.
+     *
+     * @param {object} config Configuration object.
+     */
+    constructor(config) {
+      super(config);
+
+      this.#target = Uris.checkBasicUri(config.target);
     }
 
-    for (const r of redirects) {
-      // Drop the final `/` from `toUri`, so we don't end up with double-slashes
-      // in the redirect responses.
-      const toUri = r.toUri.replace(/[/]$/, '');
-      const redirector = (req, res) => {
-        res.redirect(`${toUri}${req.path}`);
-      };
-
-      router.all(literalPath(r.fromPath), redirector);
-    }
-
-    return router;
-  }
-
-  /**
-   * Validates the given configuration object.
-   *
-   * @param {object} config Configuration object.
-   */
-  static #validateConfig(config) {
-    const validator = new JsonSchema('Redirector Configuration');
-
-    // Validator for `fromPath`, mostly done by treating it as the path part of
-    // a URL.
-    validator.addFormat('fromPath', {
-      validate: (input) => {
-        // Must start with a slash.
-        if (! /^[/]/.test(input)) {
-          return false;
-        }
-
-        try {
-          const url = new URL(`http://x${input}`);
-          return (url.pathname === input)
-            && (! /[/]{2}/.test(url.pathname)) // No empty components.
-            && (/[/]$/.test(url.pathname));    // Path ends with a slash.
-        } catch {
-          return false;
-        }
-      }
-    });
-
-    // Additional restrictions on `toUri`, beyond basic URI syntax.
-    validator.addFormat('toUri', {
-      validate: (input) => {
-        try {
-          const url = new URL(input);
-          return (/^https?:$/.test(url.protocol))
-            && (url.username === '')
-            && (url.password === '')
-            && (input.endsWith(url.pathname))
-            && (! /[/]{2}/.test(url.pathname)) // No empty components.
-            && (/[/]$/.test(url.pathname));    // Path ends with a slash.
-        } catch {
-          return false;
-        }
-      }
-    });
-
-    validator.addMainSchema({
-      $id: '/RedirectApplication',
-      type: 'object',
-      required: ['redirects'],
-      properties: {
-        redirects: {
-          type: 'array',
-          minItems: 1,
-          uniqueItems: true,
-          items: {
-            type: 'object',
-            required: ['fromPath', 'toUri'],
-            properties: {
-              fromPath: {
-                type: 'string',
-                format: 'fromPath'
-              },
-              toUri: {
-                allOf: [
-                  {
-                    type: 'string',
-                    format: 'uri'
-                  },
-                  {
-                    type: 'string',
-                    format: 'toUri'
-                  }
-                ]
-              }
-            }
-          }
-        }
-      }
-    });
-
-    const error = validator.validate(config);
-
-    if (error) {
-      error.logTo(console);
-      error.throwError();
+    /** @returns {string} The target base URI. */
+    get target() {
+      return this.#target;
     }
   }
 }
