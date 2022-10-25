8dbd429d8eae48e73c6b181f3b6cc2339e3991d0
Dan Bornstein <danfuzz@milk.com>
2022-10-25 12:41:19 -0700

    Add `TypedItem`.

---
+++ src/pkg/app-config/export/TypedItem.js
@@ -0,0 +1,38 @@
+// Copyright 2022 Dan Bornstein. All rights reserved.
+// All code and assets are considered proprietary and unlicensed.
+
+import { NamedItem } from '#x/NamedItem';
+import { Names } from '#x/Names';
+
+/**
+ * Class for configuration representations of things that each have a
+ * unique-to-its-domain name and a not-necessarily-unique type (represented as a
+ * type _name_).
+ *
+ * Accepted configuration bindings (in the constructor). All are required:
+ *
+ * * Bindings as defined by the superclass, {@link NamedItem}.
+ * * `{string} type` -- The (name of the) type of the item.
+ */
+export class TypedItem extends NamedItem {
+  /** @type {string} The (name of the) type of the item. */
+  #type;
+
+  /**
+   * Constructs an instance.
+   *
+   * @param {object} config Configuration, per the class description.
+   */
+  constructor(config) {
+    super(config);
+
+    const { type } = config;
+
+    this.#type = Names.checkType(type);
+  }
+
+  /** @returns {string} The (name of the) type of the item. */
+  get type() {
+    return this.#type;
+  }
+}
--- src/pkg/app-config/index.js
+++ src/pkg/app-config/index.js
@@ -8,5 +8,6 @@ export * from '#x/MountItem';
 export * from '#x/NamedItem';
 export * from '#x/Names';
 export * from '#x/ServerItem';
+export * from '#x/TypedItem';
 export * from '#x/Uris';
 export * from '#x/Util';
