a4c422920383cad56a185418cbcce55273fa6f84
Dan Bornstein <danfuzz@milk.com>
2022-10-26 09:58:55 -0700

    Add `checkFileName()`.

--- src/pkg/app-config/export/Files.js
+++ src/pkg/app-config/export/Files.js
@@ -22,6 +22,16 @@ export class Files {
   }
 
   /**
+   * @returns {string} Regex pattern for a plain file name (no directories),
+   * anchored at both ends so as to only match complete strings. This allows
+   * any non-empty string that has no slashes (`/`), _except_ the two strings
+   * `.` ("this directory") and `..` ("the parent directory").
+   */
+  static get FILE_NAME_PATTERN() {
+    return '^(?![.]{1,2}$)[^/]+$';
+  }
+
+  /**
    * Checks that a given value is a string matching {@link
    * #ABSOLUTE_PATH_PATTERN}.
    *
@@ -31,5 +41,17 @@ export class Files {
    */
   static checkAbsolutePath(value) {
     return MustBe.string(value, this.ABSOLUTE_PATH_PATTERN);
+  }
+
+  /**
+   * Checks that a given value is a string matching {@link
+   * #FILE_NAME_PATTERN}.
+   *
+   * @param {*} value Value in question.
+   * @returns {string} `value` if it is a string which matches the pattern.
+   * @throws {Error} Thrown if `value` does not match.
+   */
+  static checkFileName(value) {
+    return MustBe.string(value, this.FILE_NAME_PATTERN);
   }
 }
