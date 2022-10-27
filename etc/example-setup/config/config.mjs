14e7d468a0e2d2b120acc946af09e2dbf9c35af2
Dan Bornstein <danfuzz@milk.com>
2022-10-27 10:32:47 -0700

    JS version of config file.

---
+++ etc/example-setup/config/config.mjs
@@ -0,0 +1,128 @@
+import * as fs from 'node:fs/promises';
+
+const fileUrl  = (path) => new URL(path, import.meta.url);
+const filePath = (path) => fileUrl(path).pathname;
+const readFile = async (path) => fs.readFile(fileUrl(path));
+
+// Application definitions.
+const applications = [
+  {
+    name:   'my-wacky-redirector',
+    type:   'redirect-server',
+    target: 'https://milk.com/boop/'
+  },
+  {
+    name:       'my-static-fun',
+    type:       'static-server',
+    assetsPath: filePath('../assets')
+  }
+];
+
+// Service definitions.
+const services = [
+  {
+    name:      'syslog',
+    type:      'system-logger',
+    directory: filePath('../../../log'),
+    baseName:  'system'
+  },
+  {
+    name:      'requests',
+    type:      'request-logger',
+    directory: filePath('../../../log'),
+    baseName:  'requests'
+  },
+  {
+    name:        'limiter',
+    type:        'rate-limiter',
+    connections: {
+      maxBurstSize: 5,
+      flowRate:     1,
+      timeUnit:     'second',
+      maxQueueSize: 15
+    },
+    requests: {
+      maxBurstSize: 20,
+      flowRate:     5,
+      timeUnit:     'second',
+      maxQueueSize: 100
+    },
+    data: {
+      maxBurstSize:      500000,
+      flowRate:          10000,
+      timeUnit:          'second',
+      maxQueueGrantSize: 10000,
+      maxQueueSize:      1000000
+    }
+  }
+];
+
+// Host / certificate bindings.
+const hosts = [
+  {
+    hostnames:   ['localhost', '*'],
+    certificate: await readFile('localhost-cert.pem'),
+    privateKey:  await readFile('localhost-key.pem')
+  }
+];
+
+// Server defintions, including mount points for applications.
+const servers = [
+  {
+    name:          'insecure',
+    hostnames:     ['*'],
+    interface:     '*',
+    port:          8080,
+    protocol:      'http',
+    rateLimiter:   'limiter',
+    requestLogger: 'requests',
+    mounts: [
+      {
+        application: 'my-wacky-redirector',
+        at:          '//*/'
+      }
+    ]
+  },
+  {
+    name:          'secure',
+    hostnames:     ['*'],
+    interface:     '*',
+    port:          8443,
+    protocol:      'http2',
+    rateLimiter:   'limiter',
+    requestLogger: 'requests',
+    mounts: [
+      {
+        application: 'my-static-fun',
+        at:          '//*/'
+      },
+      {
+        application: 'my-static-fun',
+        at:          '//*/florp/'
+      }
+    ]
+  },
+  {
+    name:          'also-secure',
+    hostnames:     ['*'],
+    interface:     '*',
+    port:          8444,
+    protocol:      'https',
+    requestLogger: 'requests',
+    mounts: [
+      {
+        application: 'my-static-fun',
+        at:          '//*/'
+      }
+    ]
+  }
+];
+
+const config = {
+  applications,
+  hosts,
+  servers,
+  services
+};
+
+export default config;
