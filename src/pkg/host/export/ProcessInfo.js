51d1b96ef28616d64418338e6d3c6c6d0cc81139
Dan Bornstein <danfuzz@milk.com>
2022-11-01 10:06:20 -0700

    Add `ProcessInfo`.

---
+++ src/pkg/host/export/ProcessInfo.js
@@ -0,0 +1,49 @@
+// Copyright 2022 Dan Bornstein. All rights reserved.
+// All code and assets are considered proprietary and unlicensed.
+
+import * as fs from 'node:fs';
+
+import { FormatUtils } from '@this/loggy';
+
+import { Dirs } from '#x/Dirs';
+
+
+/**
+ * Utilities for getting at information about the process that is running this
+ * system.
+ */
+export class ProcessInfo {
+  /** @type {?object} All the info, if calculated. */
+  static #info = null;
+
+  /** @returns {object} All process info. */
+  static get allInfo() {
+    this.#makeInfo();
+    return { ...this.#info };
+  }
+
+  /**
+   * Initializes this class.
+   */
+  static init() {
+    this.#makeInfo();
+  }
+
+  /**
+   * Makes {@link #info} if not yet done.
+   */
+  static #makeInfo() {
+    if (this.#info) {
+      return;
+    }
+
+    const startTimeMsec = Date.now() (process.uptime() * 1000);
+    const startTimeSecs = startTimeMsec / 1000;
+    const startTime     = FormatUtils.dateTimeStringFromSecs(startTimeSecs);
+
+    const pid           = process.pid;
+    const ppid          = process.ppid;
+
+    this.#info = { pid, ppid, startTime, startTimeSecs };
+  }
+}
