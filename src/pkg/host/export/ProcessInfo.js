7e4cb4d939d4ec95545fa5ce01b004bebebef1e8
Dan Bornstein <danfuzz@milk.com>
2022-11-01 10:39:23 -0700

    Fix copy-paste travesty.

--- src/pkg/host/export/ProcessInfo.js
+++ src/pkg/host/export/ProcessInfo.js
@@ -1,11 +1,7 @@
 // Copyright 2022 Dan Bornstein. All rights reserved.
 // All code and assets are considered proprietary and unlicensed.
 
-import * as fs from 'node:fs';
-
 import { FormatUtils } from '@this/loggy';
-
-import { Dirs } from '#x/Dirs';
 
 
 /**
