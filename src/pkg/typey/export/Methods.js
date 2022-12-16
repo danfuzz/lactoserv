072560eecd18f14e7942b5a191fdfdc1ae3b33ef
Dan Bornstein <danfuzz@milk.com>
2022-12-15 16:54:36 -0800

    Copyright tweakage.

--- src/pkg/typey/export/Methods.js
+++ src/pkg/typey/export/Methods.js
@@ -1,4 +1,4 @@
-// Copyright 2022 Dan Bornstein. All rights reserved.
+// Copyright 2022 the Lactoserv Authors (Dan Bornstein et alia).
 // All code and assets are considered proprietary and unlicensed.
 
 /**
