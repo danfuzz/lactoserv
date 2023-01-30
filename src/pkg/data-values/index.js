b58cb43c7974bf687f8dc1796a3ee5a1a00d5b8f
Dan Bornstein <danfuzz@milk.com>
2023-01-30 15:25:44 -0800

    First cut of new module `data-values`.

---
+++ src/pkg/data-values/index.js
@@ -0,0 +1,6 @@
+// Copyright 2022 the Lactoserv Authors (Dan Bornstein et alia).
+// This project is PROPRIETARY and UNLICENSED.
+
+export * from '#x/Construct';
+export * from '#x/DataValues';
+export * from '#x/NonData';
