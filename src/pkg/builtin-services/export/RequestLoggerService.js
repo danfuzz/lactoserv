364f756990afe33790a64f0ec3b78f0e35c1ec4f
Dan Bornstein <danfuzz@milk.com>
2023-02-02 15:59:48 -0800

    Add rotation support to the request logger.

--- src/pkg/builtin-services/export/RequestLoggerService.js
+++ src/pkg/builtin-services/export/RequestLoggerService.js
@@ -5,6 +5,7 @@ import * as fs from 'node:fs/promises';
 
 import { FileServiceConfig } from '@this/app-config';
 import { BaseService, ServiceController } from '@this/app-framework';
+import { Rotator } from '@this/app-util';
 import { IntfRequestLogger } from '@this/network-protocol';
 
 
@@ -23,6 +24,9 @@ export class RequestLoggerService extends BaseService {
   /** @type {string} Full path to the log file. */
   #logFilePath;
 
+  /** @type {?Rotator} File rotator to use, if any. */
+  #rotator;
+
   /**
    * Constructs an instance.
    *
@@ -33,21 +37,30 @@ export class RequestLoggerService extends BaseService {
     super(config, controller);
 
     this.#logFilePath = config.resolvePath();
+    this.#rotator     = config.rotate ? new Rotator(config, this.logger) : null;
   }
 
   /** @override */
   async logCompletedRequest(line) {
     await fs.appendFile(this.#logFilePath, `${line}\n`);
+    await this.#rotator?.onWrite();
   }
 
   /** @override */
-  async start() {
+  async start(isReload) {
     await this.config.createDirectoryIfNecessary();
+    if (isReload) {
+      this.#rotator?.onReload();
+    } else {
+      this.#rotator?.onStart();
+    }
   }
 
   /** @override */
-  async stop() {
-    // Nothing to do here.
+  async stop(willReload) {
+    if (!willReload) {
+      this.#rotator?.onStop();
+    }
   }
 
 
