830242403d61fb69dc9663c010db9c4e36fd9e02
Dan Bornstein <danfuzz@milk.com>
2023-02-01 17:35:37 -0800

    Add custom inspectors.

--- src/pkg/data-values/export/Construct.js
+++ src/pkg/data-values/export/Construct.js
@@ -1,6 +1,9 @@
 // Copyright 2022 the Lactoserv Authors (Dan Bornstein et alia).
 // This project is PROPRIETARY and UNLICENSED.
 
+import * as util from 'node:util';
+
+
 /**
  * Data value that represents the construction of a behavior-bearing object.
  *
@@ -75,5 +78,42 @@ export class Construct {
    */
   withConvertedValue(innerValue) {
     return new Construct(...innerValue);
+  }
+
+  /**
+   * Custom inspector for instances of this class.
+   *
+   * @param {number} depth Maximum depth to inspect to.
+   * @param {object} options Inspection options.
+   * @param {Function} inspect Inspector function to use for sub-inspection.
+   * @returns {string} The inspected form.
+   */
+  [util.inspect.custom](depth, options, inspect) {
+    if (depth < 0) {
+      return '[Construct]';
+    }
+
+    const innerOptions = Object.assign({}, options, {
+      depth: (options.depth === null) ? null : options.depth - 1
+    });
+
+    const parts = [
+      'new ',
+      inspect(this.#type, innerOptions),
+      '('
+    ];
+
+    let first = true;
+    for (const arg of this.#args) {
+      if (first) {
+        first = false;
+      } else {
+        parts.push(', ');
+      }
+      parts.push(inspect(arg, innerOptions));
+    }
+
+    parts.push(')');
+    return parts.join('');
   }
 }
