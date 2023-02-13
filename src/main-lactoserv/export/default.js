ccb4f8e0cfa2acfd63313bfb354c5e0e89d0ab9e
Dan Bornstein <danfuzz@milk.com>
2023-02-13 11:01:29 -0800

    Simplify.

--- src/main-lactoserv/export/default.js
+++ src/main-lactoserv/export/default.js
@@ -5,12 +5,11 @@ import { Host, KeepRunning } from '@this/host';
 
 import { Debugging } from '#p/Debugging';
 import { MainArgs } from '#p/MainArgs';
-import { SystemInit } from '#p/SystemInit';
 import { UsualSystem } from '#p/UsualSystem';
 
 
 export default async function main() {
-  SystemInit.init();
+  Host.init();
 
   const args = new MainArgs(process.argv);
   args.parse();
