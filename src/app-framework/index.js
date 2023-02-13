54d41c2f96460cc274378d61a07a49b68beb012a
Dan Bornstein <danfuzz@milk.com>
2023-02-13 14:37:11 -0800

    New class `ComponentManager`.
    ...a generalization of the other two `*Manager` classes.

--- src/app-framework/index.js
+++ src/app-framework/index.js
@@ -6,6 +6,7 @@ export * from '#x/BaseApplication';
 export * from '#x/BaseComponent';
 export * from '#x/BaseControllable';
 export * from '#x/BaseService';
+export * from '#x/ComponentManager';
 export * from '#x/ComponentRegistry';
 export * from '#x/HostManager';
 export * from '#x/NetworkServer';
