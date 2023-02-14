424b782feb255451340174b9e270fa2504a1c494
Dan Bornstein <danfuzz@milk.com>
2023-02-14 12:23:22 -0800

    Add `handleRequestAsync()`.
    ...along with scaffolding to handle apps that haven't been fixed.

--- src/app-framework/export/BaseApplication.js
+++ src/app-framework/export/BaseApplication.js
@@ -4,6 +4,7 @@
 import * as timers from 'node:timers';
 
 import { ApplicationConfig } from '@this/app-config';
+import { ManualPromise } from '@this/async';
 import { BaseLoggingEnvironment, FormatUtils, IntfLogger } from '@this/loggy';
 import { WranglerContext } from '@this/network-protocol';
 import { Methods } from '@this/typey';
@@ -31,6 +32,107 @@ export class BaseApplication extends BaseComponent {
     super(config, logger);
 
     this.#loggingEnv = this.logger?.$env ?? null;
+  }
+
+  /**
+   * Asks this instance's underlying application to handle the given request.
+   * Parameters are as defined by the Express middleware spec, except that
+   * instead of a `next` callback argument, this method async-returns to
+   * indicate the status of the request. Specifically:
+   *
+   * * Returning `true` means that the request was fully handled. (In regular
+   *   Express, this is achieved by not-calling `next()` at all.)
+   * * Returning `false` means that the request was not handled at all. (In
+   *   regular Express, this is achieved by calling `next()` or `next('route')`,
+   *   that is, with no argument or the single argument `'route'`.)
+   * * Throwing an error means that the request failed fatally. (In regular
+   *   Express, this is achieved by calling `next(error)`, that is, passing it
+   *   an `Error` object.)
+   *
+   * **Note:** Express differentiates between `next()` and `next('route')`, but
+   * the nature of this system is that there is no distinction, because there
+   * are no sub-chained routes. To achieve that effect, a concrete subclass of
+   * this class would instead perform its own internal route chaining.
+   *
+   * @param {object} req Request object.
+   * @param {object} res Response object.
+   * @returns {boolean} Was the request handled? Flag as described above.
+   */
+  async handleRequestAsync(req, res) {
+    let startTime;
+    let id;
+
+    if (this.logger) {
+      startTime = this.#loggingEnv.nowSec();
+      id        = WranglerContext.get(req)?.id;
+      this.logger.handling(id, req.url);
+    }
+
+    const result = this._impl_handleRequestAsync(req, res);
+
+    if (this.logger) {
+      // Arrange to log about the result of the `_impl_` call once it settles.
+      (async () => {
+        let eventType;
+        let error = [];
+
+        try {
+          const settled = await result;
+          eventType = settled ? 'handled' : 'notHandled';
+        } catch (e) {
+          error = [e];
+          eventType = 'threw';
+        }
+
+        const endTime  = this.#loggingEnv.nowSec();
+        const duration = endTime - startTime;
+        const durStr   = FormatUtils.durationStringFromSecs(duration);
+        this.logger[eventType](id, durStr, ...error);
+      })();
+    }
+
+    return result;
+  }
+
+  /**
+   * Handles a request, as defined by the Express middleware spec and the
+   * public method on this class {@link #handleRequestAsync}.
+   *
+   * @param {object} req Request object.
+   * @param {object} res Response object.
+   */
+  async _impl_handleRequestAsync(req, res) {
+    // TODO: This should be an abstract method. What's here right now is
+    // scaffolding to convert from async back to callback-style handling.
+    this.logger?.SCAFFOLDING_BACK_TO_CALLBACK();
+
+    const resultMp = new ManualPromise();
+    const origEnd  = res.end;
+
+    const next = (arg = null) => {
+      res.end = origEnd;
+      if ((arg === null) || (arg === 'route')) {
+        resultMp.resolve(false);
+      } else if (arg instanceof Error) {
+        resultMp.reject(arg);
+      } else {
+        resultMp.reject(new Error(`Strange value passed to \`next()\`: ${arg}`));
+      }
+    };
+
+    res.end = (...args) => {
+      res.end = origEnd;
+      res.end(...args);
+      resultMp.resolve(true);
+    };
+
+    try {
+      this._impl_handleRequest(req, res, next);
+    } catch (e) {
+      resultMp.reject(e);
+    }
+
+    return resultMp.promise;
   }
 
   /**
@@ -101,7 +203,7 @@ export class BaseApplication extends BaseComponent {
    * @abstract
    * @param {object} req Request object.
    * @param {object} res Response object.
-   * @param {function(?object=)} next Function which causes the next-bound
+   * @param {function(?object)} next Function which causes the next-bound
    *   middleware to run.
    */
   _impl_handleRequest(req, res, next) {
