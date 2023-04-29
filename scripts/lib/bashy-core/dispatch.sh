f5cdc8bfa7812196905edc5c64d08b9a60ac34a9
Dan Bornstein <danfuzz@milk.com>
2023-04-29 16:48:55 -0700

    Document. Remove now-unused bits.

--- scripts/lib/bashy-core/dispatch.sh
+++ scripts/lib/bashy-core/dispatch.sh
@@ -2,8 +2,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 #
-# Script dispatch helper. This is included by exposed scripts (e.g., directly
-# under the `scripts` directory), to implement dispatch to sub-library scripts.
+# Script dispatch helper. This can be used by scripts to invoke other scripts.
+# It is particularly useful as the main call to implement a top-level "run some
+# subcommand" script (`lib --libs=<my-project> "$@"`).
 #
 
 # The directory holding all sub-libraries.
@@ -59,6 +60,12 @@ function include-lib {
 #   search. Without this, all sublibraries are searched.
 # * `--path` -- Prints the path of the script instead of running it.
 # * `--quiet` -- Does not print error messages.
+#
+# After the options, the next argument is taken to be a main command. After
+# that, any number of subcommands are accepted as long as they are allowed by
+# the main command. See the docs for more details on directory structure. TLDR:
+# A subcommand is a directory with a `_run` script in it along with any number
+# of other executable scripts or subcommand directories.
 function lib {
     _dispatch_initLibNames || return "$?"
 
@@ -103,97 +110,10 @@ function lib {
     fi
 }
 
-# Performs dispatch. Accepts any number of sub-library names as arguments,
-# followed by the argument `--` and then the script name, options, and arguments
-# to run. The script name is looked up in the sub-libraries, and if something
-# matching is found, it is dispatched to. The sub-libraries are searched in the
-# order given.
-#
-# A "script name" does not have to be a single word: Sub-libraries can define
-# a command hierarchy via directories named as if they were scripts. Each such
-# directory can contain multiple scripts _or_ further script directories. In
-# addition, each such directory must define a script called literally `_run`, to
-# be run if no further sub-commands are listed on the original commandline.
-function lib-dispatch {
-    local libDirs=()
-    while (( $# > 0 )); do
-        if [[ $1 == '--' ]]; then
-            shift
-            break
-        fi
-        libDirs+=("${_dispatch_libDir}/$1")
-        shift
-    done
-
-    if (( $# == 0 )); then
-        error-msg 'Missing subcommand name.'
-        return 1
-    fi
-
-    local cmdName="$1"
-
-    local libDir path
-    for libDir in "${libDirs[@]}"; do
-        path="${libDir}/${cmdName}"
-        if [[ -x ${path} ]]; then
-            _dispatch_dispatch-in-dir "${libDir}" "$@"
-            return "$?"
-        fi
-    done
-
-    error-msg "Subcommand not found: ${cmdName}"
-    return 1
-}
-
 
 #
 # Library-internal functions
 #
-
-# Performs dispatch in the given directory.
-function _dispatch_dispatch-in-dir {
-    local libDir="$1"
-    local cmdName="$2"
-    shift 2
-
-    local cmdWords=("${cmdName}")
-    local path="${libDir}/${cmdName}"
-
-    while true; do
-        if [[ ! -x "${path}" ]]; then
-            # Error: We landed at a non-exsitent path, unexecutable file, or
-            # unsearchable directory.
-            break
-        elif [[ -f ${path} ]]; then
-            _dispatch_run-script "${path}" "${cmdWords[*]}" "$@"
-            return "$?"
-        elif [[ -d ${path} ]]; then
-            local subCmdName="$1"
-            local subPath="${path}/${subCmdName}"
-            if _dispatch_is-valid-name "${subCmdName}" && [[ -x "${subPath}" ]]; then
-                # The next word is a valid next subcommand. Iterate.
-                shift
-                cmdWords+=("${subCmdName}")
-                path="${subPath}"
-            elif [[ -f "${path}/_run" && -x "${path}/_run" ]]; then
-                # The next word isn't a subcommand name, but there's a `_run`
-                # in the innermost subcommand directory. Run it.
-                _dispatch_run-script "${path}/_run" "${cmdWords[*]}" "$@"
-            else
-                # Error: The next word isn't a subcommand name, and there's no
-                # `default` to fall back on.
-                cmdWords+=("${subCmdName}")
-                break
-            fi
-        else
-            # Error: We landed at a special file (device, etc.).
-            break
-        fi
-    done
-
-    error-msg "Subcommand not found: ${cmdWords[*]}"
-    return 1
-}
 
 # Finds the named library script, based on the given commandline arguments. This
 # uses variables to communicate with its caller (both for efficiency and
