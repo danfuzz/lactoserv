689a258176c02e496ad58ec660a5694d198fef88
Dan Bornstein <danfuzz@milk.com>
2023-06-01 11:26:30 -0700

    No longer needed.

--- upstream-deps/bashy-lib/AUTHORS.md
+++
@@ -1,7 +0,0 @@
-Authors
-=======
-
-* Dan Bornstein:
-  [Email](mailto:danfuzz@milk.com) /
-  [Website](https://milk.com/) /
-  [GitHub](https://github.com/danfuzz)
--- upstream-deps/bashy-lib/example/scripts/lib/my-project/sub/_init.sh
+++
@@ -1,9 +0,0 @@
-# Copyright 2022-2023 the Bashy-lib Authors (Dan Bornstein et alia).
-# SPDX-License-Identifier: Apache-2.0
-
-#
-# This is an example subcommand initialization file.
-#
-
-. "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/../_init.sh" \
-|| return "$?"
