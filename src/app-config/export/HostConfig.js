f4b4bbd3015f7505b594162509e36f612ccfe8b2
Dan Bornstein <danfuzz@murtbo.com>
2023-12-13 11:15:39 -0800

    Add `selfSigned`.

--- src/app-config/export/HostConfig.js
+++ src/app-config/export/HostConfig.js
@@ -1,6 +1,8 @@
 // Copyright 2022-2023 the Lactoserv Authors (Dan Bornstein et alia).
 // SPDX-License-Identifier: Apache-2.0
 
+import { MustBe } from '@this/typey';
+
 import { BaseConfig } from '#x/BaseConfig';
 import { Certificates } from '#x/Certificates';
 import { Uris } from '#x/Uris';
@@ -11,16 +13,19 @@ import { Util } from '#x/Util';
  * Configuration representation for a "host" item, that is, a thing that
  * defines the mapping from one or more names to a certificate / key pair.
  *
+ * Accepted configuration bindings (in the constructor).
+ *
  * * `{string|string[]} hostnames` -- Names of the hosts associated with this
  *   entry. Names can in the form `*.<name>` to match any subdomain of `<name>`,
  *   or `*` to be a complete wildcard (that is, matches any name not otherwise
- *   mentioned).
+ *   mentioned). Required.
  * * `{string|Buffer} certificate` -- The certificate chain for `hostnames`, as
- *   PEM-encoded data.
+ *   PEM-encoded data. Required if `selfSigned` is absent or `false`.
  * * `{string|Buffer} privateKey` -- The private key associated with
- *   `certificate`, as PEM-encoded data.
- *
- * Accepted configuration bindings (in the constructor). All are required:
+ *   `certificate`, as PEM-encoded data. Required if `selfSigned` is absent or
+ *   `false`.
+ * * `{boolean} selfSigned` -- Optional indicator of whether this entry should
+ *   use a self-signed certificate.
  */
 export class HostConfig extends BaseConfig {
   /** @type {string[]} The hostnames in question. */
@@ -32,6 +37,9 @@ export class HostConfig extends BaseConfig {
   /** @type {string} The private key, as PEM-encoded data. */
   #privateKey;
 
+  /** @type {boolean} Is this to be a self-signed certificate? */
+  #selfSigned;
+
   /**
    * Constructs an instance.
    *
@@ -40,11 +48,26 @@ export class HostConfig extends BaseConfig {
   constructor(config) {
     super(config);
 
-    const { hostnames, certificate, privateKey } = config;
+    const { hostnames, certificate, privateKey, selfSigned = false } = config;
 
-    this.#hostnames   = Util.checkAndFreezeStrings(hostnames, Uris.HOSTNAME_PATTERN);
-    this.#certificate = Certificates.checkCertificateChain(HostConfig.#bufferFilter(certificate));
-    this.#privateKey  = Certificates.checkPrivateKey(HostConfig.#bufferFilter(privateKey));
+    this.#hostnames  = Util.checkAndFreezeStrings(hostnames, Uris.HOSTNAME_PATTERN);
+    this.#selfSigned = MustBe.boolean(selfSigned);
+
+    if (selfSigned) {
+      if ((certificate !== undefined) && (certificate !== null)) {
+        throw new Error('Cannot use `certificate` with `selfSigned === true`.');
+      }
+      if ((privateKey !== undefined) && (privateKey !== null)) {
+        throw new Error('Cannot use `certificate` with `selfSigned === true`.');
+      }
+      this.#certificate = null;
+      this.#privateKey  = null;
+    } else {
+      this.#certificate =
+        Certificates.checkCertificateChain(HostConfig.#bufferFilter(certificate));
+      this.#privateKey =
+        Certificates.checkPrivateKey(HostConfig.#bufferFilter(privateKey));
+    }
   }
 
   /**
@@ -55,15 +78,27 @@ export class HostConfig extends BaseConfig {
     return this.#hostnames;
   }
 
-  /** @returns {string} The certificate, as PEM-encoded data. */
+  /**
+   * @returns {?string} The certificate as PEM-encoded data, or `null` if
+   * {@link #selfSigned} is `true`.
+   */
   get certificate() {
     return this.#certificate;
   }
 
-  /** @returns {string} The private key, as PEM-encoded data. */
+  /**
+   * @returns {?string} The private key as PEM-encoded data, or `null` if
+   * {@link #selfSigned} is `true`.
+   */
   get privateKey() {
     return this.#privateKey;
   }
+
+  /** @returns {boolean} Is this entry to use a self-signed certificate? */
+  get selfSigned() {
+    return this.#selfSigned;
+  }
+
 
   //
   // Static members
