2344143ccab96035058c59c6257e9e0127526283
Dan Bornstein <danfuzz@murtbo.com>
2024-01-10 16:54:44 -0800

    First cut of a new app, `SimpleResponse`.

---
+++ src/builtin-applications/export/SimpleResponse.js
@@ -0,0 +1,162 @@
+// Copyright 2022-2024 the Lactoserv Authors (Dan Bornstein et alia).
+// SPDX-License-Identifier: Apache-2.0
+
+import fs from 'node:fs/promises';
+
+import { ApplicationConfig, Files } from '@this/app-config';
+import { BaseApplication } from '@this/app-framework';
+import { FsUtil } from '@this/fs-util';
+import { IntfLogger } from '@this/loggy';
+import { MimeTypes } from '@this/net-util';
+import { MustBe } from '@this/typey';
+
+
+/**
+ * Simple response server. See docs for configuration object details.
+ */
+export class SimpleResponse extends BaseApplication {
+  /** @type {?(Buffer|string)} Body contents of the response, if known. */
+  #body = null;
+
+  /** @type {?object} Send options to use, if known. */
+  #sendOptions = null;
+
+  /**
+   * Constructs an instance.
+   *
+   * @param {ApplicationConfig} config Configuration for this application.
+   * @param {?IntfLogger} logger Logger to use, or `null` to not do any logging.
+   */
+  constructor(config, logger) {
+    super(config, logger);
+
+    this.#body = config.body;
+  }
+
+  /** @override */
+  async _impl_handleRequest(request, dispatch_unused) {
+    const result =
+      await request.sendContent(this.#body, SimpleResponse.#SEND_OPTIONS);
+
+    return result;
+  }
+
+  /** @override */
+  async _impl_start(isReload_unused) {
+    if (!this.#body) {
+      const filePath = this.config.filePath;
+
+      if (!await FsUtil.fileExists(filePath)) {
+        throw new Error(`Not found or not a file: ${filePath}`);
+      }
+
+      this.#body = await fs.readFile(filePath);
+    }
+
+    this.#sendOptions = {
+      ...SimpleResponse.#SEND_OPTIONS,
+      contentType: this.config.mimeType
+    };
+  }
+
+  /** @override */
+  async _impl_stop(willReload_unused) {
+    // Nothing to do here.
+  }
+
+
+  //
+  // Static members
+  //
+
+  /** @type {object} File sending/serving configuration options. */
+  static #SEND_OPTIONS = Object.freeze({
+    maxAgeMsec: 5 * 60 * 1000 // 5 minutes.
+  });
+
+  /** @override */
+  static get CONFIG_CLASS() {
+    return this.#Config;
+  }
+
+  /**
+   * Configuration item subclass for this (outer) class.
+   */
+  static #Config = class Config extends ApplicationConfig {
+    /**
+     * @type {?string} MIME type of the response, or `null` to infer it from
+     * {@link #filePath}.
+     */
+    #mimeType;
+
+    /**
+     * @type {?(string|Buffer)} Body contents of the response, or `null` to
+     * use {@link #filePath}.
+     */
+    #body = null;
+
+    /**
+     * @type {?string} Absolute path to a file for the body contents, or `null`
+     * if {@link #body} is supplied directly.
+     */
+    #filePath = null;
+
+    /**
+     * Constructs an instance.
+     *
+     * @param {object} config Configuration object.
+     */
+    constructor(config) {
+      super(config);
+
+      const { body = null, filePath = null, type = null } = config;
+
+      if (body !== null) {
+        if (type === null) {
+          throw new Error('Must supply `type` if `body` is used.');
+        } else if (filePath !== null) {
+          throw new Error('Cannot specify both `body` and `filePath`.');
+        }
+      } else if (filePath === null) {
+        throw new Error('Must specify one of `body` or `filePath`.');
+      }
+
+      this.#mimeType = (type === null)
+        ? MimeTypes.typeFromExtensionOrType(type)
+        : MimeTypes.typeFromExtension(filePath);
+
+      if (filePath !== null) {
+        this.#filePath = Files.checkAbsolutePath(filePath);
+      } else {
+        if (!(body instanceof Buffer)) {
+          MustBe.string(body);
+        }
+        this.#body = body;
+      }
+    }
+
+    /**
+     * @returns {?(string|Buffer)} Body contents of the response, or `null` to
+     * use {@link #filePath}.
+     */
+    get body() {
+      return this.#body;
+    }
+
+    /**
+     * @returns {?string} MIME type of the response, or `null` to infer it from
+     * {@link #filePath}.
+     */
+    get mimeType() {
+      return this.#mimeType;
+    }
+
+    /**
+     * @returns {?string} Absolute path to a file for the body contents, or
+     * `null` if {@link #body} is supplied directly.
+     */
+    get filePath() {
+      return this.#filePath;
+    }
+  };
+}
