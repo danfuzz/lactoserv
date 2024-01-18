a7329abe164c845d08d88df5171636deefd89bdc
Dan Bornstein <danfuzz@murtbo.com>
2024-01-18 11:56:50 -0800

    Add tests.

--- src/net-util/tests/HostInfo.test.js
+++ src/net-util/tests/HostInfo.test.js
@@ -44,7 +44,7 @@ describe('constructor', () => {
 
 // TODO: nameKey
 
-describe('nameString', () => {
+describe('.nameString', () => {
   test('gets the name that was passed in the constructor', () => {
     const name = 'floop.florp';
     const hi   = new HostInfo(name, 123);
@@ -53,7 +53,7 @@ describe('nameString', () => {
   });
 });
 
-describe('portNumber', () => {
+describe('.portNumber', () => {
   test('gets the port number-per-se that was passed in the constructor', () => {
     const port = 5432;
     const hi   = new HostInfo('host', port);
@@ -69,7 +69,7 @@ describe('portNumber', () => {
   });
 });
 
-describe('portString', () => {
+describe('.portString', () => {
   test('gets the string form of the port number-per-se that was passed in the constructor', () => {
     const port = 99;
     const hi   = new HostInfo('host', port);
@@ -85,7 +85,51 @@ describe('portString', () => {
   });
 });
 
-// TODO: nameIsIpAddress
-// TODO: localhostInstance
+describe('nameIsIpAddress()', () => {
+  test('returns `true` given an IPv4 address for the name', () => {
+    const hi = new HostInfo('1.2.3.4', 123);
+    expect(hi.nameIsIpAddress()).toBeTrue();
+  });
+
+  test('returns `true` given an IPv6 address for the name', () => {
+    const hi = new HostInfo('[1234:abcd::ef]', 123);
+    expect(hi.nameIsIpAddress()).toBeTrue();
+  });
+
+  // Various `false` cases.
+  test.each`
+  arg
+  ${'a'}
+  ${'floop'}
+  ${'x.y'}
+  ${'12.x'}
+  ${'x.12'}
+  ${'x.12.y'}
+  `('returns `false` given the name $arg', ({ arg }) => {
+    const hi = new HostInfo(arg, 123);
+    expect(hi.nameIsIpAddress()).toBeFalse();
+  });
+});
+
+describe('localhostInstance()', () => {
+  describe.each`
+  protocol   | port
+  ${'http'}  | ${80}
+  ${'https'} | ${443}
+  ${'http2'} | ${443}
+  `('for protocol $protocol (port $port)', ({ protocol, port }) => {
+    test('constructs an instance with name `localhost`', () => {
+      const hi = HostInfo.localhostInstance(protocol);
+      expect(hi.nameString).toBe('localhost');
+    });
+
+    test('constructs an instance with the expected port', () => {
+      const hi = HostInfo.localhostInstance(protocol);
+      expect(hi.portNumber).toBe(port);
+      expect(hi.portString).toBe(port.toString());
+    });
+  });
+});
+
 // TODO: parseHostHeader
 // TODO: safeParseHostHeader
