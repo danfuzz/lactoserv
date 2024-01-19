706a4eac2ce4bd02dccc26d2fce98bc27573bdca
Dan Bornstein <danfuzz@murtbo.com>
2024-01-19 12:31:53 -0800

    Remove unused `import`s.

--- src/async/export/TokenBucket.js
+++ src/async/export/TokenBucket.js
@@ -1,10 +1,8 @@
 // Copyright 2022-2024 the Lactoserv Authors (Dan Bornstein et alia).
 // SPDX-License-Identifier: Apache-2.0
 
-import * as timers from 'node:timers/promises';
-
 import { IntfTimeSource, StdTimeSource } from '@this/metacomp';
-import { Methods, MustBe } from '@this/typey';
+import { MustBe } from '@this/typey';
 
 import { ManualPromise } from '#x/ManualPromise';
 import { Threadlet } from '#x/Threadlet';
