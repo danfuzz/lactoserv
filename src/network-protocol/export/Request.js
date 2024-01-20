2579bd30a005df8f0964979dd541d985952ea4cd
Dan Bornstein <danfuzz@murtbo.com>
2024-01-20 13:03:42 -0800

    Remove temporary debugging code.

--- src/network-protocol/export/Request.js
+++ src/network-protocol/export/Request.js
@@ -105,20 +105,6 @@ export class Request {
       this.#id     = logger.$meta.makeId();
       this.#logger = logger[this.#id];
     }
-
-    if (!/^[/]/.test(request.url)) {
-      // Sanity check. If we end up here, it's a bug and not (in particular) a
-      // malformed request (which never should have made it this far).
-      // TODO: In practice this is happening, and it's not clear why. Log it,
-      // so we can figure out what's going on.
-      this.#logger.strangeOriginalUrl({
-        hostname: request.hostname,
-        method:   request.method,
-        protocol: request.protocol,
-        url:      request.url
-      });
-      throw new Error(`Shouldn't happen: ${request.url}`);
-    }
   }
 
   /**
