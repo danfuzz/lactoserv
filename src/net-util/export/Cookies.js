fd3de5611020935ce16f9feecb4cffeca859bef8
Dan Bornstein <danfuzz@murtbo.com>
2024-01-24 12:36:17 -0800

    No longer need this package.

--- src/net-util/export/Cookies.js
+++ src/net-util/export/Cookies.js
@@ -1,8 +1,6 @@
 // Copyright 2022-2024 the Lactoserv Authors (Dan Bornstein et alia).
 // SPDX-License-Identifier: Apache-2.0
 
-import cookie from 'cookie';
-
 import { MustBe } from '@this/typey';
 
 
