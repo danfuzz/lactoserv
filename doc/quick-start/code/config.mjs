483d8f947d714824ee3ea2fda1d7ace6ba932ae8
Dan Bornstein <danfuzz@murtbo.com>
2024-02-09 08:42:14 -0800

    The `URL` tactic works for everyone.

--- doc/quick-start/code/config.mjs
+++ doc/quick-start/code/config.mjs
@@ -1,12 +1,5 @@
+const SITE_DIR = new URL('../website', import.meta.url).pathname;
 
-let SITE_DIR = process.env.SITE_DIR;
-
-if (!SITE_DIR) {
-  // `SITE_DIR` is only set in the "standalone" example.
-  SITE_DIR = new URL('../website', import.meta.url).pathname;
-}
-
-console.log('####################', SITE_DIR);
 const config = {
   hosts: [{ hostnames: ['localhost'], selfSigned: true }],
   applications: [
