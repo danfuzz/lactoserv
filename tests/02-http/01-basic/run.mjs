47c565637d0e82a6a4ddf5cc776ee3567f8f5d6f
Dan Bornstein <danfuzz@murtbo.com>
2024-02-13 15:01:26 -0800

    Remove redundancy.

--- tests/02-http/01-basic/run.mjs
+++ tests/02-http/01-basic/run.mjs
@@ -3,64 +3,6 @@
 
 import { checkResponse } from '@this/integration-testing';
 
-async function xcheckResponse(responsePromise, config) {
-  const response = await responsePromise;
-
-  console.log('### Metadata');
-
-  let anyErrors = false;
-
-  if (response.status !== config.status) {
-    console.log('* Status: got %s, expected %s', response.status, config.status);
-    anyErrors = true;
-  }
-
-  if (response.statusText !== config.statusText) {
-    console.log('* Status text: got %s, expected %s', response.statusText, config.statusText);
-    anyErrors = true;
-  }
-
-  const gotHeaders = new Map(response.headers.entries());
-  for (const [name, value] of Object.entries(config.headers)) {
-    const got = gotHeaders.get(name);
-    gotHeaders.delete(name);
-
-    if (got === null) {
-      console.log('* Header %s: missing', name);
-      anyErrors = true;
-    } else if (typeof value === 'string') {
-      if (got !== value) {
-        console.log('* Header %s: got %o, expected `%o`', name, got, value);
-        anyErrors = true;
-      }
-    } else {
-      if (!new RegExp(value).test(got)) {
-        console.log('* Header %s: got `%o`, expected match of `/%o/`', name, got, value);
-        anyErrors = true;
-      }
-    }
-  }
-
-  for (const [name, value] of Object.entries(gotHeaders)) {
-    console.log('* Header %s: unexpected, value `%o`', name, value);
-    anyErrors = true;
-  }
-
-  if (!anyErrors) {
-    console.log('* No problems.');
-  }
-
-  console.log('');
-  console.log('### Body');
-  console.log('');
-
-  if (!response.body) {
-    console.log('(no body)');
-  } else {
-    console.log('```\n%s```', await response.text());
-  }
-}
-
 const response = await fetch('http://localhost:8080/', { redirect: 'manual' });
 
 await checkResponse(response, {
