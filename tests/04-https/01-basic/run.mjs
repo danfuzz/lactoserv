55cb636ff73c42d491579f0ff7bb4e06b6968166
Dan Bornstein <danfuzz@murtbo.com>
2024-02-13 15:13:01 -0800

    Fix.

--- tests/04-https/01-basic/run.mjs
+++ tests/04-https/01-basic/run.mjs
@@ -3,17 +3,19 @@
 
 import { checkResponse } from '@this/integration-testing';
 
-const response = await fetch('https://localhost:8444/');
+const response = await fetch('https://localhost:8443/');
 
 await checkResponse(response, {
   status: 200,
   statusText: 'OK',
   headers: {
+    'accept-ranges':  'bytes',
     'cache-control':  'public, max-age=300',
     'connection':     'keep-alive',
     'content-length': '637',
     'content-type':   'text/html; charset=utf-8',
     'date':           /^[,: a-zA-Z0-9]+ GMT$/,
+    'etag':           /^"[-0-9a-zA-Z]+"$/,
     'server':         /^lactoserv-[.0-9]+ [0-9a-f]+$/
   }
 });
