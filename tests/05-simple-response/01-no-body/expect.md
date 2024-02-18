335d3dc1616b98830bb187cc4085f44af329e3ec
Dan Bornstein <danfuzz@murtbo.com>
2024-02-18 10:56:10 -0800

    Add tests.

--- tests/05-simple-response/01-no-body/expect.md
+++ tests/05-simple-response/01-no-body/expect.md
@@ -7,3 +7,23 @@
 ### Body
 
 No body, as expected.
+
+## ETag conditional, matching
+
+### Metadata
+
+* No problems.
+
+### Body
+
+No body, as expected.
+
+## ETag conditional, unmatching
+
+### Metadata
+
+* No problems.
+
+### Body
+
+No body, as expected.
--- tests/05-simple-response/01-no-body/run.mjs
+++ tests/05-simple-response/01-no-body/run.mjs
@@ -5,7 +5,7 @@ import { requestAndCheck } from '@this/integration-testing';
 
 const theUrl = 'https://localhost:8443/resp/no-body';
 
-await requestAndCheck(
+const result = await requestAndCheck(
   'Unconditional',
   {
     url: theUrl
@@ -14,7 +14,41 @@ await requestAndCheck(
     statusText: 'No Content',
     headers: {
       'cache-control': 'public, immutable, max-age=660',
-      'etag':          /^"[-+/0-9a-zA-Z]+"$/,
+      'etag':          /^"[-+/0-9a-zA-Z]+"$/
+    },
+    body: null
+  });
+
+await requestAndCheck(
+  'ETag conditional, matching',
+  {
+    url: theUrl,
+    headers: {
+      'if-none-match': result.headers.get('etag')
+    }
+  }, {
+    status: 304,
+    statusText: 'Not Modified',
+    headers: {
+      'cache-control': 'public, immutable, max-age=660',
+      'etag':          /^"[-+/0-9a-zA-Z]+"$/
+    },
+    body: null
+  });
+
+await requestAndCheck(
+  'ETag conditional, unmatching',
+  {
+    url: theUrl,
+    headers: {
+      'if-none-match': '"floop-bloop"'
+    }
+  }, {
+    status: 204,
+    statusText: 'No Content',
+    headers: {
+      'cache-control': 'public, immutable, max-age=660',
+      'etag':          result.headers.get('etag')
     },
     body: null
   });
