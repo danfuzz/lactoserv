54b89d97b418f5ba4a33493eb041d43a6f94de23
Dan Bornstein <danfuzz@murtbo.com>
2024-02-29 09:35:16 -0800

    Reflow.

--- src/async/export/TokenBucket.js
+++ src/async/export/TokenBucket.js
@@ -88,36 +88,34 @@ export class TokenBucket {
    *   rate), that is, how quickly the bucket gets filled, in tokens per second.
    *   This defines the steady state "flow rate" allowed by the instance. Must
    *   be a finite positive number. This is a required "option."
-   * @param {number} [options.initialBurstSize] The
-   *   instantaneously available burst size, in tokens, at the moment of
-   *   construction. Defaults to `maxBurstSize` (that is, able to be maximally
-   *   "bursted" from the get-go).
+   * @param {number} [options.initialBurstSize] The instantaneously available
+   *   burst size, in tokens, at the moment of construction. Defaults to
+   *   `maxBurstSize` (that is, able to be maximally "bursted" from the get-go).
    * @param {number} options.maxBurstSize Maximum possible instantaneous burst
    *   size (that is, the total bucket capacity in the "leaky bucket as meter"
    *   metaphor), in tokens (arbitrary volume units). This defines the
    *   "burstiness" allowed by the instance. Must be a finite positive number.
    *   This is a required "option."
-   * @param {number} [options.maxQueueGrantSize] Maximum
-   *   grant size when granting requests from the waiter queue, in tokens. No
-   *   queued grant requests will ever return a larger grant, even if there is
-   *   available "burst volume" to accommodate it. Must be a finite non-negative
-   *   number less than or equal to both `maxBurstSize` and `maxQueueSize`, or
-   *   `null` to indicate the default. If `partialTokens === false`, then the
-   *   value is rounded down to an integer by `Math.floor()`. If `0`, then this
-   *   instance will only ever synchronously grant tokens. Defaults to the
-   *   smaller of `maxBurstSize` or `maxQueueSize`.
-   * @param {?number} [options.maxQueueSize] The maximum allowed waiter
-   *   queue size, in tokens. Must be a finite non-negative number or `null`.
-   *   If `null`, then there is no limit on the queue size. If `0`, then this
+   * @param {number} [options.maxQueueGrantSize] Maximum grant size when
+   *   granting requests from the waiter queue, in tokens. No queued grant
+   *   requests will ever return a larger grant, even if there is available
+   *   "burst volume" to accommodate it. Must be a finite non-negative number
+   *   less than or equal to both `maxBurstSize` and `maxQueueSize`, or `null`
+   *   to indicate the default. If `partialTokens === false`, then the value is
+   *   rounded down to an integer by `Math.floor()`. If `0`, then this instance
+   *   will only ever synchronously grant tokens. Defaults to the smaller of
+   *   `maxBurstSize` or `maxQueueSize`.
+   * @param {?number} [options.maxQueueSize] The maximum allowed waiter queue
+   *   size, in tokens. Must be a finite non-negative number or `null`. If
+   *   `null`, then there is no limit on the queue size. If `0`, then this
    *   instance will only ever synchronously grant tokens.
-   * @param {boolean} [options.partialTokens] If `true`, allows the
-   *   instance to provide partial tokens (e.g. give a client `1.25` tokens). If
-   *   `false`, all token handoffs from the instance are quantized to integer
-   *   values.
-   * @param {IntfTimeSource} options.timeSource What to use to
-   *   determine the passage of time. If not specified, the instance will use a
-   *   standard implementation which measures time in seconds (_not_ msec) and
-   *   bottoms out at the usual JavaScript / Node wall time interface (e.g.
+   * @param {boolean} [options.partialTokens] If `true`, allows the instance to
+   *   provide partial tokens (e.g. give a client `1.25` tokens). If `false`,
+   *   all token handoffs from the instance are quantized to integer values.
+   * @param {IntfTimeSource} options.timeSource What to use to determine the
+   *   passage of time. If not specified, the instance will use a standard
+   *   implementation which measures time in seconds (_not_ msec) and bottoms
+   *   out at the usual JavaScript / Node wall time interface (e.g.
    *   `Date.now()`, `timers.setTimeout()`).
    */
   constructor(options) {
