407756695adf5696ee7e4a1e7ccc46563875932d
Dan Bornstein <danfuzz@murtbo.com>
2024-03-15 10:21:18 -0700

    Collate some more.

--- src/jest.config.mjs
+++ src/jest.config.mjs
@@ -22,6 +22,38 @@ export default {
     forceCoverageMatch: ['**/@this/**']
   },
 
+  // Test-finding options.
+  ...{
+    testMatch: [
+      '**/code/node_modules/**/tests/**/*.test.{js,cjs,mjs}'
+    ],
+
+    // Without explicitly setting this to an empty array, `**/node_modules/**`
+    // would be ignored (which is exactly where all our tests are).
+    testPathIgnorePatterns: []
+  },
+
+  // Directory-setup options.
+  ...{
+    // Without this, there is a "secret" ignore pattern which prevents any files
+    // under a `node_modules` directory from being found.
+    haste: {
+      retainAllFiles: true
+    },
+
+    // This allows Jest to find the Jest-related modules that are used in the
+    // testing environment (e.g. `jest-extended`).
+    modulePaths: [
+      `${TESTER_DIR}/lib/node_modules`
+    ],
+
+    // That is, point at the built code, which includes external dependencies.
+    rootDir: `${OUT_PROJECT_DIR}/lib`,
+
+    // This has needed adjustment in the past.
+    // roots: ['<rootDir>']
+  },
+
   // Make calling deprecated APIs throw helpful error messages
   // errorOnDeprecated: false,
 
@@ -31,20 +63,6 @@ export default {
   // A path to a module which exports an async function that is triggered once after all test suites
   // globalTeardown: undefined,
 
-  // Without this, there is a "secret" ignore pattern which prevents any files
-  // under a `node_modules` directory from being found.
-  haste: {
-    retainAllFiles: true
-  },
-
-  // This allows Jest to find the Jest-related modules that are used in the
-  // testing environment (e.g. `jest-extended`).
-  modulePaths: [
-    `${TESTER_DIR}/lib/node_modules`
-  ],
-
-  rootDir: `${OUT_PROJECT_DIR}/lib`, // That is, point at the built output.
-  // roots: ['<rootDir>'] // This has needed adjustment in the past.
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
   setupFiles: [
@@ -60,14 +78,5 @@ export default {
   setupFilesAfterEnv: [
     `${TESTER_DIR}/lib/node_modules/jest-extended/all`,
     `${TESTER_DIR}/lib/code/node_modules/@this/main-tester`
-  ],
-
-  // Glob patterns that match all test files.
-  testMatch: [
-    '**/code/node_modules/**/tests/**/*.test.{js,cjs,mjs}'
-  ],
-
-  // Without explicitly setting this to an empty array, `**/node_modules/**`
-  // would be ignored (which is exactly where all our tests are).
-  testPathIgnorePatterns: []
+  ]
 };
