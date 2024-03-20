fc99b77fca8e60f2df3ba45f6a7f3373dab18078
Dan Bornstein <danfuzz@murtbo.com>
2024-03-20 10:03:22 -0700

    No longer need these roll-up classes.

--- src/builtin-applications/index.js
+++ src/builtin-applications/index.js
@@ -1,7 +1,6 @@
 // Copyright 2022-2024 the Lactoserv Authors (Dan Bornstein et alia).
 // SPDX-License-Identifier: Apache-2.0
 
-export * from '#x/BuiltinApplications';
 export * from '#x/Redirector';
 export * from '#x/SimpleResponse';
 export * from '#x/StaticFiles';
--- src/builtin-services/index.js
+++ src/builtin-services/index.js
@@ -1,7 +1,6 @@
 // Copyright 2022-2024 the Lactoserv Authors (Dan Bornstein et alia).
 // SPDX-License-Identifier: Apache-2.0
 
-export * from '#x/BuiltinServices';
 export * from '#x/MemoryMonitor';
 export * from '#x/ProcessIdFile';
 export * from '#x/ProcessInfoFile';
