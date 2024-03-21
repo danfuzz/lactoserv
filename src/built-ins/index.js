e06300085b337a6a4313a50ac1a4c325f8ac0f48
Dan Bornstein <danfuzz@murtbo.com>
2024-03-21 12:18:57 -0700

    First cut of new class `HostRouter`.

--- src/built-ins/index.js
+++ src/built-ins/index.js
@@ -1,6 +1,7 @@
 // Copyright 2022-2024 the Lactoserv Authors (Dan Bornstein et alia).
 // SPDX-License-Identifier: Apache-2.0
 
+export * from '#x/HostRouter';
 export * from '#x/MemoryMonitor';
 export * from '#x/PathRouter';
 export * from '#x/ProcessIdFile';
