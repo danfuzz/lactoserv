3f09c85f28283125894e46e403d0598155305d98
Dan Bornstein <danfuzz@murtbo.com>
2024-03-21 16:10:19 -0700

    Remove unused `import`.

--- src/net-util/tests/Uris.test.js
+++ src/net-util/tests/Uris.test.js
@@ -1,7 +1,6 @@
 // Copyright 2022-2024 the Lactoserv Authors (Dan Bornstein et alia).
 // SPDX-License-Identifier: Apache-2.0
 
-import { TreePathKey } from '@this/collections';
 import { Uris } from '@this/net-util';
 
 
