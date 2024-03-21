d13ad6ea44893bedbe5218597133d38c91e4d46f
Dan Bornstein <danfuzz@murtbo.com>
2024-03-21 11:09:10 -0700

    First cut of new class `PathRouter`.

--- src/built-ins/index.js
+++ src/built-ins/index.js
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export * from '#x/MemoryMonitor';
+export * from '#x/PathRouter';
 export * from '#x/ProcessIdFile';
 export * from '#x/ProcessInfoFile';
 export * from '#x/RateLimiter';
