8347c36fc74920d8de42c7461344173e43da1f48
Dan Bornstein <danfuzz@murtbo.com>
2024-04-01 13:25:30 -0700

    Pulled `{Rotate,Save}Config` into `BaseFileService` as inner classes.

--- src/sys-config/index.js
+++ src/sys-config/index.js
@@ -1,7 +1,5 @@
 // Copyright 2022-2024 the Lactoserv Authors (Dan Bornstein et alia).
 // SPDX-License-Identifier: Apache-2.0
 
-export * from '#x/RotateConfig';
-export * from '#x/SaveConfig';
 export * from '#x/ServiceConfig';
 export * from '#x/ServiceUseConfig';
