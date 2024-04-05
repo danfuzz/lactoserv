74dcdb4cdb8928b549e0c25d33420bbbd624d7f6
Dan Bornstein <danfuzz@murtbo.com>
2024-04-05 16:32:20 -0700

    Sort imports.

--- src/loggy/export/TextFileSink.js
+++ src/loggy/export/TextFileSink.js
@@ -2,7 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { EventSink, LinkedEvent } from '@this/async';
-import { BaseConverter, Converter, ConverterConfig, Duration } from '@this/data-values';
+import { BaseConverter, Converter, ConverterConfig, Duration }
+  from '@this/data-values';
 import { FileAppender } from '@this/fs-util';
 import { LogPayload } from '@this/loggy-intf';
 import { MustBe } from '@this/typey';
