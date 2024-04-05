d428d5c1d954054cd5be73c73d248e8bc21c9a82
Dan Bornstein <danfuzz@murtbo.com>
2024-04-05 13:42:44 -0700

    Add missing `import`.

--- src/host/export/KeepRunning.js
+++ src/host/export/KeepRunning.js
@@ -1,7 +1,7 @@
 // Copyright 2022-2024 the Lactoserv Authors (Dan Bornstein et alia).
 // SPDX-License-Identifier: Apache-2.0
 
-import { BaseExposedThreadlet } from '@this/async';
+import { BaseExposedThreadlet, Threadlet } from '@this/async';
 import { WallClock } from '@this/clocks';
 import { IntfLogger } from '@this/loggy-intf';
 
