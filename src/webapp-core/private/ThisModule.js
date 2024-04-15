ec1e77ff0cb22af1f6c2dcb9111b5a979ca4243a
Dan Bornstein <danfuzz@murtbo.com>
2024-04-15 13:50:20 -0700

    Remove defunct logger bits.

--- src/webapp-core/private/ThisModule.js
+++ src/webapp-core/private/ThisModule.js
@@ -11,13 +11,6 @@ import { MustBe } from '@this/typey';
  */
 export class ThisModule {
   /**
-   * Map of all loggers returned from {@link #loggerFor}.
-   *
-   * @type {Map<string, IntfLogger>}
-   */
-  static #loggers = new Map();
-
-  /**
    * Base logger for this module's subsystems, or `null` not to do any logging.
    *
    * @type {?IntfLogger}
@@ -30,56 +23,5 @@ export class ThisModule {
    */
   static get logger() {
     return this.#logger;
-  }
-
-  /**
-   * Gets a logger for a particular cohort. A "cohort" is a set of similar items
-   * of some sort, e.g. "applications" or "services."
-   *
-   * @param {string} name Name of the cohort.
-   * @returns {?IntfLogger} Logger for the cohort, or `null` if it is not to be
-   *   logged.
-   */
-  static cohortLogger(name) {
-    return this.#loggerFor(name, true);
-  }
-
-  /**
-   * Gets a logger for a particular subsystem.
-   *
-   * @param {string} name Name of the subsystem or cohort.
-   * @returns {?IntfLogger} Logger for the subsystem, or `null` if it is not to
-   *   be logged.
-   */
-  static subsystemLogger(name) {
-    return this.#loggerFor(name, false);
-  }
-
-  /**
-   * Gets a logger for a particular subsystem or cohort. (A "cohort" is a set of
-   * loggers for similar items, e.g. "applications" or "services.")
-   *
-   * @param {string} name Name of the subsystem or cohort.
-   * @param {boolean} isCohort Is this a cohort?
-   * @returns {?IntfLogger} Logger for the subsystem or cohort, or `null` if it
-   *   is not to be logged.
-   */
-  static #loggerFor(name, isCohort) {
-    MustBe.string(name);
-
-    const mapKey  = `${name}-${isCohort}`;
-    const already = this.#loggers.get(mapKey);
-
-    if (already) {
-      return already;
-    }
-
-    const result = isCohort
-      ? Loggy.loggerFor(name)
-      : this.#logger?.[name];
-
-    this.#loggers.set(mapKey, result);
-
-    return result;
   }
 }
