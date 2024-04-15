c1490e32ec44f8aaba5fe995b5648b2f2371e960
Dan Bornstein <danfuzz@murtbo.com>
2024-04-15 08:52:30 -0700

    Add `isName()` and `parsePath()`.

--- src/compote/export/Names.js
+++ src/compote/export/Names.js
@@ -1,7 +1,8 @@
 // Copyright 2022-2024 the Lactoserv Authors (Dan Bornstein et alia).
 // SPDX-License-Identifier: Apache-2.0
 
-import { AskIf } from '@this/typey';
+import { TreePathKey } from '@this/collections';
+import { MustBe } from '@this/typey';
 
 
 /**
@@ -20,12 +21,57 @@ export class Names {
    * @throws {Error} Thrown if `value` does not match.
    */
   static checkName(value) {
-    const pattern = /^(?![-.])[-_.+a-zA-Z0-9]+(?<![-.])$/;
-
-    if (AskIf.string(value, pattern)) {
+    if (this.isName(value)) {
       return value;
-    } else {
-      throw new Error(`Invalid component name: ${value}`);
     }
+
+    throw new Error(`Invalid component name: ${value}`);
+  }
+
+  /**
+   * Checks whether or not a given value is a string which can be used as a
+   * "name of something" in this system. Allowed strings must be non-empty and
+   * consist only of alphanumerics plus any of `-_.+`, and furthermore must
+   * start and end with an alphanumeric character or `_`.
+   *
+   * @param {string} value Value in question.
+   * @returns {boolean} `true` if it is a string which matches the stated
+   *   pattern, or `false` if not.
+   * @throws {Error} Thrown if `value` is not even a string.
+   */
+  static isName(value) {
+    MustBe.string(value);
+
+    const pattern = /^(?![-.])[-_.+a-zA-Z0-9]+(?<![-.])$/;
+    return pattern.test(value);
+  }
+
+  /**
+   * Parses an absolute component path from a slash-separated list of elements,
+   * which must begin with a slash.
+   *
+   * @param {string} path The absolute path to parse.
+   * @returns {TreePathKey} The parsed path.
+   */
+  static parsePath(path) {
+    MustBe.string(path);
+
+    const elements = path.split('/');
+    const len      = elements.length;
+
+    if ((len < 2) || (elements[0] !== '')) {
+      throw new Error(`Not an absolute component path: ${path}`);
+    }
+
+    elements.pop(); // Drop the initial empty element.
+
+    for (const e of elements) {
+      if (!this.isName(e)) {
+        throw new Error(`Not an absolute component path: ${path}`);
+      }
+    }
+
+    // `Object.freeze()` means `TreePathKey` can avoid making a copy.
+    return new TreePathKey(Object.freeze(elements), false);
   }
 }
