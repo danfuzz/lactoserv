2d7ec5a2f174f9052d8da5490fe68af3b1a10d52
Dan Bornstein <danfuzz@murtbo.com>
2024-04-24 17:35:42 -0700

    Remove class `BaseExposedThreadlet`.

--- src/async/index.js
+++ src/async/index.js
@@ -1,7 +1,6 @@
 // Copyright 2022-2024 the Lactoserv Authors (Dan Bornstein et alia).
 // SPDX-License-Identifier: Apache-2.0
 
-export * from '#x/BaseExposedThreadlet';
 export * from '#x/Condition';
 export * from '#x/EventPayload';
 export * from '#x/EventSink';
