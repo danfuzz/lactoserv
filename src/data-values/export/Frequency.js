6e21327cc56d8871c7307dba5f2b48a03854ea90
Dan Bornstein <danfuzz@murtbo.com>
2024-04-26 15:34:12 -0700

    Remove unused `import`.

--- src/data-values/export/Frequency.js
+++ src/data-values/export/Frequency.js
@@ -1,8 +1,6 @@
 // Copyright 2022-2024 the Lactoserv Authors (Dan Bornstein et alia).
 // SPDX-License-Identifier: Apache-2.0
 
-import { MustBe } from '@this/typey';
-
 import { Duration } from '#x/Duration';
 import { UnitQuantity } from '#x/UnitQuantity';
 
