31b09036aa93287dff8479131854c77a5628e146
Dan Bornstein <danfuzz@murtbo.com>
2024-04-29 12:25:45 -0700

    Add tests for `Method`.

---
+++ src/typey/tests/Methods.test.js
@@ -0,0 +1,22 @@
+// Copyright 2022-2024 the Lactoserv Authors (Dan Bornstein et alia).
+// SPDX-License-Identifier: Apache-2.0
+
+import { Methods } from '@this/typey';
+
+
+describe('abstract()', () => {
+  for (let i = 0; i < 5; i++) {
+    let label;
+    switch (i) {
+      case 0:  { label = 'no arguments';   break; }
+      case 1:  { label = 'one argument';   break; }
+      default: { label = `${i} arguments`; break; }
+    }
+    test(`throws given ${label}.`, () => {
+      const args = new Array(i);
+      args.fill('florp');
+
+      expect(() => Methods.abstract(...args)).toThrow(/Abstract method/);
+    });
+  }
+});
