2d0cde667735abf93399e8eabe72feb127c06db4
Dan Bornstein <danfuzz@murtbo.com>
2024-05-07 15:47:19 -0700

    Add tests.

--- src/compy/tests/Names.test.js
+++ src/compy/tests/Names.test.js
@@ -1,6 +1,7 @@
 // Copyright 2022-2024 the Lactoserv Authors (Dan Bornstein et alia).
 // SPDX-License-Identifier: Apache-2.0
 
+import { PathKey } from '@this/collections';
 import { Names } from '@this/compy';
 
 describe.each`
@@ -66,5 +67,81 @@ ${'isName'}     | ${false}
     } else {
       expect(got).toBeTrue();
     }
+  });
+});
+
+describe('parsePath()', () => {
+  const wildKey    = new PathKey(['a', 'b', 'c'], true);
+  const nonWildKey = new PathKey(['cd', 'ef'], false);
+
+  function nonWildcardCases(allowWildcard) {
+    test.each`
+    arg             | expected
+    ${'/'}          | ${[]}
+    ${'/a'}         | ${['a']}
+    ${'/a/b'}       | ${['a', 'b']}
+    ${'/a/b/c'}     | ${['a', 'b', 'c']}
+    ${'/boop/beep'} | ${['boop', 'beep']}
+    ${[]}           | ${[]}
+    ${['zz']}       | ${['zz']}
+    ${['zz', 'yy']} | ${['zz', 'yy']}
+    ${nonWildKey}   | ${['cd', 'ef']}
+    `('works given $arg', ({ arg, expected }) => {
+      const args = (allowWildcard === null)
+        ? [arg]
+        : [arg, allowWildcard];
+      const got = Names.parsePath(...args);
+      expect(got.wildcard).toBeFalse();
+      expect(got.path).toEqual(expected);
+    });
+  }
+
+  function wildcardCases(allowWildcard) {
+    const msg = allowWildcard
+      ? 'works given $arg'
+      : 'throws given $arg';
+    test.each`
+    arg                  | expected
+    ${'/*'}              | ${[]}
+    ${'/a/*'}            | ${['a']}
+    ${'/a/b/*'}          | ${['a', 'b']}
+    ${'/a/b/c/*'}        | ${['a', 'b', 'c']}
+    ${'/boop/beep/*'}    | ${['boop', 'beep']}
+    ${['*']}             | ${[]}
+    ${['bop', '*']}      | ${['bop']}
+    ${['bop', 'z', '*']} | ${['bop', 'z']}
+    ${wildKey}           | ${['a', 'b', 'c']}
+    `(msg, ({ arg, expected }) => {
+      if (allowWildcard) {
+        const got = Names.parsePath(arg, true);
+        expect(got.wildcard).toBeTrue();
+        expect(got.path).toEqual(expected);
+      } else if (allowWildcard === null) {
+        expect(() => Names.parsePath(arg)).toThrow();
+      } else {
+        expect(() => Names.parsePath(arg, false)).toThrow();
+      }
+    });
+  }
+
+  describe('with default `allowWildcard` (of `false`)', () => {
+    nonWildcardCases(null);
+    wildcardCases(null);
+
+    // TODO: error cases.
+  });
+
+  describe('with `allowWildcard === false`', () => {
+    nonWildcardCases(false);
+    wildcardCases(false);
+
+    // TODO: error cases.
+  });
+
+  describe('with `allowWildcard === true`', () => {
+    nonWildcardCases(true);
+    wildcardCases(true);
+
+    // TODO: error cases.
   });
 });
