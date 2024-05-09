e77628af360a8e595b661c3d2ba7070c89397c2f
Dan Bornstein <danfuzz@murtbo.com>
2024-05-09 12:51:27 -0700

    Add configurability for encoding proxies.

--- src/data-values/export/ConverterConfig.js
+++ src/data-values/export/ConverterConfig.js
@@ -101,6 +101,17 @@ export class ConverterConfig extends BaseConfig {
   }
 
   /**
+   * Action to take when asked to encode a proxy.
+   *
+   * @param {string|(function(*): *)} [value] Proposed configuration value.
+   *   Default `'wrap'`.
+   * @returns {string|(function(*): *)} Accepted configuration value.
+   */
+  _config_proxyAction(value = 'wrap') {
+    return ConverterConfig.#checkAction(value);
+  }
+
+  /**
    * Converter to handle any special cases that take precedence over other
    * configuration options, or `null` if there are no special cases.
    *
