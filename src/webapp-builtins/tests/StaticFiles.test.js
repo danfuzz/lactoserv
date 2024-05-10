c836ddae226a8c9a2b354855f3fae05c43a8e11d
Dan Bornstein <danfuzz@murtbo.com>
2024-05-10 13:09:28 -0700

    Add minimal tests.

---
+++ src/webapp-builtins/tests/StaticFiles.test.js
@@ -0,0 +1,13 @@
+// Copyright 2022-2024 the Lactoserv Authors (Dan Bornstein et alia).
+// SPDX-License-Identifier: Apache-2.0
+
+import { StaticFiles } from '@this/webapp-builtins';
+
+
+describe('constructor', () => {
+  test('accepts a valid configuration', () => {
+    expect(() => new StaticFiles({
+      siteDirectory: '/florp'
+    })).not.toThrow();
+  });
+});
