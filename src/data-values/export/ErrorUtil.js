b8254fe7fbd20bd9c8d846ed6be5080c9b256ac1
Dan Bornstein <danfuzz@murtbo.com>
2024-05-14 14:22:32 -0700

    Add `collateErrors()`.

--- src/data-values/export/ErrorUtil.js
+++ src/data-values/export/ErrorUtil.js
@@ -7,6 +7,55 @@
  */
 export class ErrorUtil {
   /**
+   * Collects and collates errors from a number of sources, producing a
+   * "uniqued" result (without any redundantly-listed info). The result includes
+   * an `ok` indicator (were there no errors?), a sorted array of unique error
+   * codes (extracted by {@link #extractErrorCode}), and an object like the one
+   * given as an argument, except replacing duplicate errors with string
+   * references.
+   *
+   * @param {object} errors Object which binds each key to a corresponding
+   *   error-or-nullish.
+   * @returns {{ errorCodes: Array<string>, errors: object, ok: boolean }}
+   *   Collected results.
+   */
+  static collateErrors(errors) {
+    const errorCodes   = new Set();
+    const errorMap     = new Map();
+    const resultErrors = {};
+
+    for (const [k, e] of Object.entries(errors)) {
+      if ((e === null) || (e === undefined)) {
+        continue;
+      }
+
+      const already = errorMap.get(e);
+
+      if (already) {
+        resultErrors[k] = already;
+      } else {
+        resultErrors[k] = e;
+        errorCodes.add(ErrorUtil.extractErrorCode(e));
+        errorMap.set(e, k);
+      }
+    }
+
+    if (errorCodes.size === 0) {
+      return {
+        errorCodes: [],
+        errors:     {},
+        ok:         true
+      };
+    } else {
+      return {
+        errorCodes: [...errorCodes].sort(),
+        errors:     resultErrors,
+        ok:         false
+      };
+    }
+  }
+
+  /**
    * Extracts a string error code from the given `Error`, or returns a generic
    * "unknown error" if there's nothing else reasonable.
    *
