86c4af2fcfd2e69cdb57efae84162eacc6e009cf
Dan Bornstein <danfuzz@murtbo.com>
2024-05-14 18:53:03 -0700

    Test all valid request methods.

--- src/net-util/tests/HttpRange.test.js
+++ src/net-util/tests/HttpRange.test.js
@@ -28,90 +28,94 @@ describe('rangeInfo()', () => {
     expect(got).toBeNull();
   });
 
-  test('works for a valid non-conditional case, explicit length', () => {
-    const requestMethod   = 'get';
-    const requestHeaders  = new HttpHeaders({ range: 'bytes=10-500' });
-    const responseHeaders = new HttpHeaders();
-    const statsOrLength   = 1000;
-    const got =
-      HttpRange.rangeInfo(requestMethod, requestHeaders, responseHeaders, statsOrLength);
+  describe.each`
+  requestMethod
+  ${'get'}
+  ${'GET'}
+  ${'head'}
+  ${'HEAD'}
+  `('given `requestMethod === \'$requestMethod\'`', ({ requestMethod }) => {
+    test('works for a valid non-conditional case, explicit length', () => {
+      const requestHeaders  = new HttpHeaders({ range: 'bytes=10-500' });
+      const responseHeaders = new HttpHeaders();
+      const statsOrLength   = 1000;
+      const got =
+        HttpRange.rangeInfo(requestMethod, requestHeaders, responseHeaders, statsOrLength);
 
-    expect(got).toEqual({
-      headers: {
-        'accept-ranges': 'bytes',
-        'content-range': 'bytes 10-500/1000'
-      },
-      status:       206,
-      start:        10,
-      end:          501,
-      endInclusive: 500,
-      length:       491
+      expect(got).toEqual({
+        headers: {
+          'accept-ranges': 'bytes',
+          'content-range': 'bytes 10-500/1000'
+        },
+        status:       206,
+        start:        10,
+        end:          501,
+        endInclusive: 500,
+        length:       491
+      });
     });
-  });
 
-  test('works for a valid non-conditional case, explicit length (bigint)', () => {
-    const requestMethod   = 'get';
-    const requestHeaders  = new HttpHeaders({ range: 'bytes=1-4' });
-    const responseHeaders = new HttpHeaders();
-    const statsOrLength   = 10n;
-    const got =
-      HttpRange.rangeInfo(requestMethod, requestHeaders, responseHeaders, statsOrLength);
+    test('works for a valid non-conditional case, explicit length (bigint)', () => {
+      const requestHeaders  = new HttpHeaders({ range: 'bytes=1-4' });
+      const responseHeaders = new HttpHeaders();
+      const statsOrLength   = 10n;
+      const got =
+        HttpRange.rangeInfo(requestMethod, requestHeaders, responseHeaders, statsOrLength);
 
-    expect(got).toEqual({
-      headers: {
-        'accept-ranges': 'bytes',
-        'content-range': 'bytes 1-4/10'
-      },
-      status:       206,
-      start:        1,
-      end:          5,
-      endInclusive: 4,
-      length:       4
+      expect(got).toEqual({
+        headers: {
+          'accept-ranges': 'bytes',
+          'content-range': 'bytes 1-4/10'
+        },
+        status:       206,
+        start:        1,
+        end:          5,
+        endInclusive: 4,
+        length:       4
+      });
     });
-  });
 
-  test('works for a valid non-conditional case, explicit length (stats object)', async () => {
-    // A convenient known-to-exist path.
-    const path  = (new URL(import.meta.url)).pathname;
-    const stats = await fs.stat(path);
-    const len   = stats.size;
+    test('works for a valid non-conditional case, explicit length (stats object)', async () => {
+      // A convenient known-to-exist path.
+      const path  = (new URL(import.meta.url)).pathname;
+      const stats = await fs.stat(path);
+      const len   = stats.size;
 
-    const requestMethod   = 'get';
-    const requestHeaders  = new HttpHeaders({ range: 'bytes=15-39' });
-    const responseHeaders = new HttpHeaders();
-    const got =
-      HttpRange.rangeInfo(requestMethod, requestHeaders, responseHeaders, stats);
+      const requestHeaders  = new HttpHeaders({ range: 'bytes=15-39' });
+      const responseHeaders = new HttpHeaders();
+      const got =
+        HttpRange.rangeInfo(requestMethod, requestHeaders, responseHeaders, stats);
 
-    expect(got).toEqual({
-      headers: {
-        'accept-ranges': 'bytes',
-        'content-range': `bytes 15-39/${len}`
-      },
-      status:       206,
-      start:        15,
-      end:          40,
-      endInclusive: 39,
-      length:       25
+      expect(got).toEqual({
+        headers: {
+          'accept-ranges': 'bytes',
+          'content-range': `bytes 15-39/${len}`
+        },
+        status:       206,
+        start:        15,
+        end:          40,
+        endInclusive: 39,
+        length:       25
+      });
     });
-  });
 
-  test('works for a valid non-conditional case, length via response header', () => {
-    const requestMethod   = 'get';
-    const requestHeaders  = new HttpHeaders({ range: 'bytes=90-109' });
-    const responseHeaders = new HttpHeaders({ 'content-length': '200' });
-    const got =
-      HttpRange.rangeInfo(requestMethod, requestHeaders, responseHeaders, null);
+    test('works for a valid non-conditional case, length via response header', () => {
+      const requestHeaders  = new HttpHeaders({ range: 'bytes=90-109' });
+      const responseHeaders = new HttpHeaders({ 'content-length': '200' });
+      const got =
+        HttpRange.rangeInfo(requestMethod, requestHeaders, responseHeaders, null);
 
-    expect(got).toEqual({
-      headers: {
-        'accept-ranges': 'bytes',
-        'content-range': 'bytes 90-109/200'
-      },
-      status:       206,
-      start:        90,
-      end:          110,
-      endInclusive: 109,
-      length:       20
+      expect(got).toEqual({
+        headers: {
+          'accept-ranges': 'bytes',
+          'content-range': 'bytes 90-109/200'
+        },
+        status:       206,
+        start:        90,
+        end:          110,
+        endInclusive: 109,
+        length:       20
+      });
     });
   });
 
