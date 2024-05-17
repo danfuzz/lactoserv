ee1b4cff66756826b58803e97cd37e91c809f58b
Dan Bornstein <danfuzz@murtbo.com>
2024-05-17 13:06:24 -0700

    Expand tests.

--- src/net-util/tests/CertUtil.test.js
+++ src/net-util/tests/CertUtil.test.js
@@ -5,14 +5,53 @@ import { CertUtil } from '@this/net-util';
 
 
 describe('checkCertificateChain()', () => {
+  const SOME_CERT =
+    '-----BEGIN CERTIFICATE-----\n' +
+    'ABCDEFG+/abcdefg1234567890=\n' +
+    '-----END CERTIFICATE-----\n';
+
   test('throws given a non-string', () => {
     expect(() => CertUtil.checkCertificateChain(12345)).toThrow();
+  });
+
+  test('throws given a string that is clearly not a certificate chain', () => {
+    expect(() => CertUtil.checkCertificateChain('florp\nflop\n')).toThrow();
+  });
+
+  test('accepts a pretty minimal syntactically correct single-cert example', () => {
+    expect(() => CertUtil.checkCertificateChain(SOME_CERT)).not.toThrow();
+  });
+
+  test('accepts a pretty minimal syntactically correct multi-cert example', () => {
+    expect(() => CertUtil.checkCertificateChain(SOME_CERT + SOME_CERT)).not.toThrow();
+    expect(() => CertUtil.checkCertificateChain(SOME_CERT + SOME_CERT + SOME_CERT)).not.toThrow();
   });
 });
 
 describe('checkPrivateKey()', () => {
+  const SOME_KEY =
+    '-----BEGIN PRIVATE KEY-----\n' +
+    'ABCDEFG+/abcdefg1234567890=\n' +
+    '-----END PRIVATE KEY-----\n';
+  const SOME_RSA_KEY =
+    '-----BEGIN RSA PRIVATE KEY-----\n' +
+    'ABCDEFG+/abcdefg1234567890=\n' +
+    '-----END RSA PRIVATE KEY-----\n';
+
   test('throws given a non-string', () => {
     expect(() => CertUtil.checkPrivateKey(12345)).toThrow();
+  });
+
+  test('throws given a string that is clearly not a private key', () => {
+    expect(() => CertUtil.checkPrivateKey('florp\nflop\n')).toThrow();
+  });
+
+  test('accepts a pretty minimal syntactically correct regular key (PKCS#8)', () => {
+    expect(() => CertUtil.checkPrivateKey(SOME_KEY)).not.toThrow();
+  });
+
+  test('accepts a pretty minimal syntactically correct RSA key (PKCS#1)', () => {
+    expect(() => CertUtil.checkPrivateKey(SOME_RSA_KEY)).not.toThrow();
   });
 });
 
