a45ea82355e661b4eb14f609edef13fdcd369a39
Dan Bornstein <danfuzz@murtbo.com>
2024-05-18 08:42:22 -0700

    Try this.

--- src/webapp-builtins/tests/RequestDelay.test.js
+++ src/webapp-builtins/tests/RequestDelay.test.js
@@ -147,7 +147,9 @@ describe('_impl_handleRequest()', () => {
     }
 
     await timeSource._end();
+    console.log('####### WAITING FOR ALL RESULTS');
     await Promise.all(results);
+    console.log('####### WAITED FOR ALL RESULTS');
     await rd.root.stop();
 
     // Make sure we got all possible values in the range. We can do this
