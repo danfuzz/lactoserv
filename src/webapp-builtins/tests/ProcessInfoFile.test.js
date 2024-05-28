a0b533af6535c92a418c9909c25011a2f116ed8c
Dan Bornstein <danfuzz@murtbo.com>
2024-05-28 16:52:01 -0700

    Add tests.

--- src/webapp-builtins/tests/ProcessInfoFile.test.js
+++ src/webapp-builtins/tests/ProcessInfoFile.test.js
@@ -10,4 +10,43 @@ describe('constructor', () => {
       path: '/florp'
     })).not.toThrow();
   });
+
+  test('accepts a valid configuration with `save`', () => {
+    expect(() => new ProcessInfoFile({
+      path: '/florp',
+      save: {
+        maxOldSize:  '1 MiB',
+        maxOldCount: 23,
+        onStart:     true,
+        onStop:      false
+      }
+    })).not.toThrow();
+  });
+
+  test('accepts a valid configuration with `rotate`', () => {
+    expect(() => new ProcessInfoFile({
+      path: '/florp',
+      rotate: {
+        atSize:      '100 KiB',
+        checkPeriod: '5 min',
+        maxOldSize:  '1 MiB',
+        maxOldCount: 99,
+        onStart:     false,
+        onStop:      true
+      }
+    })).not.toThrow();
+  });
+
+  test('rejects a configuration with both `save` and `rotate`', () => {
+    expect(() => new ProcessInfoFile({
+      path: '/florp',
+      save: {
+        maxOldCount: 10,
+        onStart:     true
+      },
+      rotate: {
+        atSize: '100 KiB'
+      }
+    })).toThrow();
+  });
 });
