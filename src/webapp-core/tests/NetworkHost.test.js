fe256557bd73890a2fc0f4395bb00c7e9044c7fa
Dan Bornstein <danfuzz@murtbo.com>
2024-06-03 15:43:08 -0700

    Add a minimal test.

---
+++ src/webapp-core/tests/NetworkHost.test.js
@@ -0,0 +1,13 @@
+// Copyright 2022-2024 the Lactoserv Authors (Dan Bornstein et alia).
+// SPDX-License-Identifier: Apache-2.0
+
+import { NetworkHost } from '@this/webapp-core';
+
+describe('constructor', () => {
+  test('accepts a valid minimal self-signed configuration', () => {
+    expect(() => new NetworkHost({
+      hostnames:  '*',
+      selfSigned: true
+    })).not.toThrow();
+  });
+});
