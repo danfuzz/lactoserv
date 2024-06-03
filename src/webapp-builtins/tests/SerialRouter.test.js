eaf16b3e5b45be06155e18c658bdabc219ed2ae6
Dan Bornstein <danfuzz@murtbo.com>
2024-06-03 12:46:17 -0700

    Add a wee bit of testing.

--- src/webapp-builtins/tests/SerialRouter.test.js
+++ src/webapp-builtins/tests/SerialRouter.test.js
@@ -5,9 +5,15 @@ import { SerialRouter } from '@this/webapp-builtins';
 
 
 describe('constructor', () => {
-  test('accepts a valid configuration', () => {
+  test('accepts a valid minimal configuration', () => {
     expect(() => new SerialRouter({
       applications: []
+    })).not.toThrow();
+  });
+
+  test('accepts a valid configuration with applications', () => {
+    expect(() => new SerialRouter({
+      applications: ['foo', 'bar', 'baz']
     })).not.toThrow();
   });
 });
