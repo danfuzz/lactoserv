fdde2e6c83aca91e008221c5da47824b927dea8c
Dan Bornstein <danfuzz@murtbo.com>
2024-06-03 15:50:20 -0700

    Add a minimal test.

---
+++ src/webapp-core/tests/NetworkEndpoint.test.js
@@ -0,0 +1,17 @@
+// Copyright 2022-2024 the Lactoserv Authors (Dan Bornstein et alia).
+// SPDX-License-Identifier: Apache-2.0
+
+import { NetworkEndpoint } from '@this/webapp-core';
+
+describe('constructor', () => {
+  test('accepts a valid configuration', () => {
+    expect(() => new NetworkEndpoint({
+      application: 'florp',
+      interface:   '*:1234',
+      protocol:    'http2',
+      services: {
+        accessLog: 'loggyLogLog'
+      }
+    })).not.toThrow();
+  });
+});
