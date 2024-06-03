9e5d993aa72bfb3cc8c03d1a4fe39897d5907e6a
Dan Bornstein <danfuzz@murtbo.com>
2024-06-03 15:37:21 -0700

    Add a very minimal test.

---
+++ src/webapp-core/tests/WebappRoot.test.js
@@ -0,0 +1,10 @@
+// Copyright 2022-2024 the Lactoserv Authors (Dan Bornstein et alia).
+// SPDX-License-Identifier: Apache-2.0
+
+import { WebappRoot } from '@this/webapp-core';
+
+describe('constructor', () => {
+  test('accepts a valid minimal configuration', () => {
+    expect(() => new WebappRoot({})).not.toThrow();
+  });
+});
