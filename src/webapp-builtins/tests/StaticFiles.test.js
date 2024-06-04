8038b2601f8333b38daac09be622c31d9742b92d
Dan Bornstein <danfuzz@murtbo.com>
2024-06-04 11:26:32 -0700

    Add tests.

--- src/webapp-builtins/tests/StaticFiles.test.js
+++ src/webapp-builtins/tests/StaticFiles.test.js
@@ -1,8 +1,12 @@
 // Copyright 2022-2024 the Lactoserv Authors (Dan Bornstein et alia).
 // SPDX-License-Identifier: Apache-2.0
 
+import { PathKey } from '@this/collections';
 import { MockRootComponent } from '@this/compy/testing';
+import { DispatchInfo, FullResponse, StatusResponse } from '@this/net-util';
 import { StaticFiles } from '@this/webapp-builtins';
+
+import { RequestUtil } from '#tests/RequestUtil';
 
 
 /**
@@ -105,7 +109,43 @@ describe('constructor', () => {
   });
 });
 
-describe('start()', () => {
+describe('_impl_handleRequest()', () => {
+  async function makeInstance() {
+    const root = new MockRootComponent();
+    const sf   = new StaticFiles({
+      siteDirectory: STATIC_SITE_DIR,
+      notFoundPath:  `${STATIC_SITE_DIR}/not-found.html`
+    });
+
+    await root.start();
+    await root.addAll(sf);
+
+    return sf;
+  }
+
+  test.each`
+  uriPath                            | filePath
+  ${'/'}                             | ${'index.html'}
+  ${'/index.html'}                   | ${'index.html'}
+  ${'/subdir1/'}                     | ${'subdir1/index.html'}
+  ${'/subdir1/index.html'}           | ${'subdir1/index.html'}
+  ${'/subdir1/some-subdir-file.txt'} | ${'subdir1/some-subdir-file.txt'}
+  ${'/subdir2/more-text.txt'}        | ${'subdir2/more-text.txt'}
+  `('given URI path `$uriPath`, retrieves file path `$filePath`', async ({ uriPath, filePath }) => {
+    const sf      = await makeInstance();
+    const request = RequestUtil.makeGet(uriPath);
+    const result  = await sf.handleRequest(request, new DispatchInfo(PathKey.EMPTY, request.pathname));
+
+    expect(result).toBeInstanceOf(FullResponse);
+    expect(result.status).toBe(200);
+
+    const body = result._testing_getBody();
+    expect(body.type).toBe('file');
+    expect(body.path).toBe(`${STATIC_SITE_DIR}/${filePath}`);
+  });
+});
+
+describe('_impl_start()', () => {
   test('accepts a `siteDirectory` that refers to an existing directory', async () => {
     const root = new MockRootComponent();
     await root.start();
