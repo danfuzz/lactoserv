d1c50bc1d4825345205e7324ac2842d0d5f44f5f
Dan Bornstein <danfuzz@murtbo.com>
2024-06-04 10:58:36 -0700

    Round out configuration tests.

--- src/webapp-builtins/tests/DataRateLimiter.test.js
+++ src/webapp-builtins/tests/DataRateLimiter.test.js
@@ -5,10 +5,26 @@ import { DataRateLimiter } from '@this/webapp-builtins';
 
 
 describe('constructor', () => {
-  test('accepts a valid configuration', () => {
+  test('accepts a valid minimal configuration', () => {
     expect(() => new DataRateLimiter({
       flowRate: '10 MiB/sec',
       maxBurst: '100 KiB'
+    })).not.toThrow();
+  });
+
+  test('accepts a valid `maxQueue`', () => {
+    expect(() => new DataRateLimiter({
+      flowRate: '10 MiB/sec',
+      maxBurst: '100 KiB',
+      maxQueue: '123 MiB'
+    })).not.toThrow();
+  });
+
+  test('accepts a valid `maxQueueGrant`', () => {
+    expect(() => new DataRateLimiter({
+      flowRate:      '10 MiB/sec',
+      maxBurst:      '100 KiB',
+      maxQueueGrant: '10000 byte'
     })).not.toThrow();
   });
 });
