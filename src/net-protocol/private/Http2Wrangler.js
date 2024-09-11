abd8351f3319ae020418d0f8fc7cd3a0c2230939
Dan Bornstein <danfuzz@murtbo.com>
2024-09-11 11:05:33 -0700

    Remove unused `import`.

--- src/net-protocol/private/Http2Wrangler.js
+++ src/net-protocol/private/Http2Wrangler.js
@@ -1,7 +1,6 @@
 // Copyright 2022-2024 the Lactoserv Authors (Dan Bornstein et alia).
 // SPDX-License-Identifier: Apache-2.0
 
-import { AsyncLocalStorage } from 'node:async_hooks';
 import * as http2 from 'node:http2';
 
 import { Condition, PromiseUtil, Threadlet } from '@this/async';
