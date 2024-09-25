6de0a47b45ffeed935df9fd9d292907f3ba5e6ad
Dan Bornstein <danfuzz@murtbo.com>
2024-09-25 12:26:19 -0700

    Post-move fixes.

--- src/structy/index.js
+++ src/structy/index.js
@@ -2,20 +2,4 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export * from '#x/BaseConfig';
-export * from '#x/BaseConverter';
-export * from '#x/BaseDataClass';
 export * from '#x/BaseStruct';
-export * from '#x/ByteCount';
-export * from '#x/ByteRate';
-export * from '#x/Converter';
-export * from '#x/ConverterConfig';
-export * from '#x/Duration';
-export * from '#x/ErrorUtil';
-export * from '#x/Frequency';
-export * from '#x/Moment';
-export * from '#x/Ref';
-export * from '#x/Sexp';
-export * from '#x/SpecialConverters';
-export * from '#x/StackFrame';
-export * from '#x/StackTrace';
-export * from '#x/UnitQuantity';
--- src/structy/tests/BaseStruct.test.js
+++ src/structy/tests/BaseStruct.test.js
@@ -1,7 +1,7 @@
 // Copyright 2022-2024 the Lactoserv Authors (Dan Bornstein et alia).
 // SPDX-License-Identifier: Apache-2.0
 
-import { BaseStruct } from '@this/data-values';
+import { BaseStruct } from '@this/structy';
 
 
 /**
