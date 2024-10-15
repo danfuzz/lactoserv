ae258a144fcb80047558213a901cb45437bcc962
Dan Bornstein <danfuzz@murtbo.com>
2024-10-15 11:30:50 -0700

    Remove now-unused `import`.

--- src/codec/tests/Codec.test.js
+++ src/codec/tests/Codec.test.js
@@ -1,7 +1,7 @@
 // Copyright 2022-2024 the Lactoserv Authors (Dan Bornstein et alia).
 // SPDX-License-Identifier: Apache-2.0
 
-import { BaseCodec, Codec, CodecConfig, Ref, Sexp } from '@this/codec';
+import { Codec, CodecConfig, Ref, Sexp } from '@this/codec';
 import { AskIf } from '@this/typey';
 
 
