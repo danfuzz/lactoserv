d70f6591b67b2599e914955055c39d1d021ad3be
Dan Bornstein <danfuzz@murtbo.com>
2024-10-21 14:20:15 -0700

    Use a visitor. Be more colorful.

--- src/loggy-intf/export/LogPayload.js
+++ src/loggy-intf/export/LogPayload.js
@@ -8,7 +8,8 @@ import { IntfDeconstructable, Sexp } from '@this/decon';
 import { Moment } from '@this/quant';
 import { Chalk } from '@this/text';
 import { MustBe } from '@this/typey';
-import { BaseDefRef, StackTrace, VisitDef } from '@this/valvis';
+import { BaseDefRef, BaseValueVisitor, StackTrace, VisitDef }
+  from '@this/valvis';
 
 import { LogTag } from '#x/LogTag';
 
@@ -143,21 +144,8 @@ export class LogPayload extends EventPayload {
    * @param {boolean} colorize Colorize the result?
    */
   #appendHumanPayload(parts, colorize) {
-    const args = this.args;
-
-    if (args.length === 0) {
-      // Avoid extra work in the easy zero-args case.
-      const text = `${this.type}()`;
-      parts.push(colorize ? chalk.bold(text) : text);
-      return;
-    }
-
-    const opener = `${this.type}(`;
-
-    parts.push(colorize ? chalk.bold(opener) : opener);
-    LogPayload.#appendHumanValue(parts, args, true);
-
-    parts.push(colorize ? chalk.bold(')') : ')');
+    const human = new LogPayload.#HumanVisitor(this, colorize).visitSync();
+    parts.push(...human.flat(Number.POSITIVE_INFINITY));
   }
 
 
@@ -298,4 +286,197 @@ export class LogPayload extends EventPayload {
       }
     }
   }
+
+  /**
+   * Visitor class which stringifies instances of this (outer) class and all its
+   * components. This produces an array whose elements are either strings or
+   * arrays (of strings or arrays of...), with the array nesting representing
+   * the hierarchical structure of instance.
+   */
+  static #HumanVisitor = class HumanVisitor extends BaseValueVisitor {
+    /**
+     * Should the output be colorized?
+     *
+     * @type {boolean}
+     */
+    #colorize;
+
+    /**
+     * Constructs an instance.
+     *
+     * @param {*} value The value to visit.
+     * @param {boolean} colorize Colorize the output?
+     */
+    constructor(value, colorize) {
+      super(value);
+      this.#colorize = colorize;
+    }
+
+    /** @override */
+    _impl_visitArray(node) {
+      return this.#visitAggregate(node, '[', ']', '[]');
+    }
+
+    /** @override */
+    _impl_visitBigInt(node_unused) {
+      throw this.#shouldntHappen();
+    }
+
+    /** @override */
+    _impl_visitBoolean(node) {
+      return `${node}`;
+    }
+
+    /** @override */
+    _impl_visitClass(node_unused) {
+      throw this.#shouldntHappen();
+    }
+
+    /** @override */
+    _impl_visitError(node_unused) {
+      throw this.#shouldntHappen();
+    }
+
+    /** @override */
+    _impl_visitFunction(node_unused) {
+      throw this.#shouldntHappen();
+    }
+
+    /** @override */
+    _impl_visitInstance(node) {
+      if (node instanceof LogPayload) {
+        const color          = chalk.bold;
+        const { type, args } = node;
+        if (args.length === 0) {
+          // Avoid extra work in the easy zero-args case.
+          const text = `${type}()`;
+          return [this.#maybeColorize(text, color)];
+        } else {
+          const open  = this.#maybeColorize(`${type}(`, color);
+          const close = this.#maybeColorize(')', color);
+          return this.#visitAggregate(args, open, close, null);
+        }
+      } else if (node instanceof BaseDefRef) {
+        const color  = chalk.magenta.bold;
+        const result = [this.#maybeColorize(`#${node.index}`, color)];
+        if (node instanceof VisitDef) {
+          result.push(
+            this.#maybeColorize(' = ', color),
+            this._prot_visit(node.value).value);
+        }
+        return result;
+      } else if (node instanceof Sexp) {
+        const result = this.#visitAggregate(node.args, '(', ')', '()');
+        return ['@', node.functorName, ...result];
+      } else {
+        throw this.#shouldntHappen();
+      }
+    }
+
+    /** @override */
+    _impl_visitNull() {
+      return 'null';
+    }
+
+    /** @override */
+    _impl_visitNumber(node) {
+      return this.#maybeColorize(`${node}`, chalk.yellow);
+    }
+
+    /** @override */
+    _impl_visitPlainObject(node) {
+      return this.#visitAggregate(node, '{ ', ' }', '{}');
+    }
+
+    /** @override */
+    _impl_visitProxy(node_unused, isFunction_unused) {
+      throw this.#shouldntHappen();
+    }
+
+    /** @override */
+    _impl_visitString(node) {
+      // `inspect()` to get good quoting, etc.
+      return this.#maybeColorize(util.inspect(node), chalk.green);
+    }
+
+    /** @override */
+    _impl_visitSymbol(node_unused) {
+      throw this.#shouldntHappen();
+    }
+
+    /** @override */
+    _impl_visitUndefined() {
+      throw this.#shouldntHappen();
+    }
+
+    /**
+     * Colorizes the given text, but only if this instance has been told to
+     * colorize.
+     *
+     * @param {string} text The text in question.
+     * @param {Function} func The colorizer function.
+     * @returns {string} The colorized-or-not result.
+     */
+    #maybeColorize(text, func) {
+      return this.#colorize ? func(text) : text;
+    }
+
+    /**
+     * Constructs a "shouldn't happen" error. This is used in the implementation
+     * of all the `_impl_visit*()` methods corresponding to types that aren't
+     * supposed to show up in a payload.
+     *
+     * @returns {Error} The error.
+     */
+    #shouldntHappen() {
+      return new Error('Shouldn\'t happen.');
+    }
+
+    /**
+     * Helper for various `_impl_visit*()` methods, which visits an aggregate
+     * object of some sort.
+     *
+     * @param {*} node The aggregate to visit.
+     * @param {string} open The "open" string.
+     * @param {string} close The "close" string.
+     * @param {string} ifEmpty The string to use to represent an empty
+     *   instance.
+     * @returns {Array<string>} The stringified aggregate, as an array.
+     */
+    #visitAggregate(node, open, close, ifEmpty) {
+      const isArray = Array.isArray(node);
+      const result  = [open];
+      let   first   = true;
+      let   inProps = !isArray;
+
+      const initialVisit = isArray
+        ? this._prot_visitArrayProperties(node)
+        : this._prot_visitObjectProperties(node);
+
+      for (const [k, v] of Object.entries(initialVisit)) {
+        if (!inProps && (k === 'length')) {
+          inProps = true;
+          continue;
+        } else if (first) {
+          first = false;
+        } else {
+          result.push(', ');
+        }
+
+        if (inProps) {
+          // `inspect()` to get good quoting, etc.
+          result.push(util.inspect(k), ': ');
+        }
+
+        result.push(v);
+      }
+
+      if (result.length === 1) {
+        return [ifEmpty];
+      } else {
+        result.push(close);
+        return result;
+      }
+    }
+  };
 }
