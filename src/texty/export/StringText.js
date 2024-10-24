f35d584cc04fe6c9a3dfcd54a7ec8e4f7ff3db4c
Dan Bornstein <danfuzz@murtbo.com>
2024-10-24 12:15:34 -0700

    "Absorb" spaces into the indentation.

--- src/texty/export/StringText.js
+++ src/texty/export/StringText.js
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { BaseText } from '#x/BaseText';
-import { TypeText } from '#x/TypeText';
 
 
 /**
@@ -35,5 +34,18 @@ export class StringText extends BaseText {
   /** @override */
   toString() {
     return this.#value;
+  }
+
+  /** @override */
+  _impl_renderMultiline(options) {
+    // What's going on: If the string is just spaces and we're at the beginning
+    // of a line, then we don't render anything. This handles cases like
+    // splitting at the commas or braces in `{ x: 10, y: 20 }`.
+
+    const { atColumn } = options;
+
+    return ((atColumn === -1) && /^ *$/.test(this.#value))
+      ? { endColumn: atColumn, value: '' }
+      : super._impl_renderMultiline(options);
   }
 }
