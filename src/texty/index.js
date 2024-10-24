2175fd26a528989ed2403f37de5f7bc830e4185a
Dan Bornstein <danfuzz@murtbo.com>
2024-10-24 12:16:06 -0700

    New class `SepText`.
    To be used soon.

--- src/texty/index.js
+++ src/texty/index.js
@@ -5,6 +5,7 @@ export * from '#x/BaseText';
 export * from '#x/Chalk';
 export * from '#x/ComboText';
 export * from '#x/IndentedText';
+export * from '#x/SepText';
 export * from '#x/StringText';
 export * from '#x/StyledText';
 export * from '#x/TypeText';
