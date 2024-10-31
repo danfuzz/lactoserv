e05b2bcda70dda0f513ffda76bf72ee213bf47bb
Dan Bornstein <danfuzz@murtbo.com>
2024-10-30 18:00:55 -0700

    Appease the linter.

--- src/net-util/export/HostInfo.js
+++ src/net-util/export/HostInfo.js
@@ -66,7 +66,7 @@ export class HostInfo {
    */
   constructor(nameString, portNumber) {
     // Note: The regex is a bit lenient. TODO: Maybe it shouldn't be?
-    this.#nameString = MustBe.string(nameString, /^[-_.:\[\]a-zA-Z0-9]+$/);
+    this.#nameString = MustBe.string(nameString, /^[-_.:[\]a-zA-Z0-9]+$/);
 
     this.#portNumber = AskIf.string(portNumber)
       ? Number(MustBe.string(portNumber, /^[0-9]+$/))
